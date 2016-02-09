4462db36238b25636b50e0300a680fc400b92276
zengzhu <zeng.zhu@gmail.com>
2016-02-09 22:18:11 +0800

    add performance test.

--- android/kreader/tests/pdfiumWrapperTest/src/com/onyx/kreader/tests/ReaderTestActivityTest.java
+++ android/kreader/tests/pdfiumWrapperTest/src/com/onyx/kreader/tests/ReaderTestActivityTest.java
@@ -2,8 +2,12 @@ package com.onyx.kreader.tests;
 
 import android.graphics.Bitmap;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import com.onyx.kreader.api.ReaderBitmap;
+import com.onyx.kreader.host.impl.ReaderBitmapImpl;
 import com.onyx.kreader.plugins.pdfium.PdfiumJniWrapper;
 import com.onyx.kreader.test.ReaderTestActivity;
+import com.onyx.kreader.utils.BitmapUtils;
 import com.onyx.kreader.utils.StringUtils;
 
 /**
@@ -17,6 +21,8 @@ import com.onyx.kreader.utils.StringUtils;
  * com.onyx.reader.tests/android.test.InstrumentationTestRunner
  */
 public class ReaderTestActivityTest extends ActivityInstrumentationTestCase2<ReaderTestActivity> {
+
+    static private String TAG = ReaderTestActivityTest.class.getSimpleName();
 
     public ReaderTestActivityTest() {
         super("com.onyx.reader", ReaderTestActivity.class);
@@ -86,6 +92,38 @@ public class ReaderTestActivityTest extends ActivityInstrumentationTestCase2<Rea
         assertTrue(wrapper.drawPage(page, 0, 0, bitmap.getWidth(), bitmap.getHeight(), bitmap));
         assertFalse(wrapper.nativePageSize(page + 1, size));
         assertTrue(wrapper.nativeCloseDocument());
+        assertTrue(wrapper.nativeDestroyLibrary());
+    }
+
+    public void testPageRenderPerformance() {
+        PdfiumJniWrapper wrapper = new PdfiumJniWrapper();
+        assertTrue(wrapper.nativeInitLibrary());
+        String [] pathes = { "/mnt/sdcard/Books/b.pdf", "/mnt/sdcard/Books/a.pdf", "/mnt/sdcard/Books/c.pdf"};
+        for(String path : pathes) {
+            assertTrue(wrapper.nativeOpenDocument(path, null) == PdfiumJniWrapper.NO_ERROR);
+            int pageCount = wrapper.nativePageCount();
+            assertTrue(pageCount > 0);
+            ReaderBitmapImpl readerBitmap = null;
+            for (int i = 0; i < pageCount; ++i) {
+                float size[] = new float[2];
+                assertTrue(wrapper.nativePageSize(i, size));
+                assertTrue(size[0] > 0);
+                assertTrue(size[1] > 0);
+                if (readerBitmap == null) {
+                    readerBitmap = new ReaderBitmapImpl((int) size[0], (int) size[1], Bitmap.Config.ARGB_8888);
+                } else {
+                    readerBitmap.update((int) size[0], (int) size[1], Bitmap.Config.ARGB_8888);
+                }
+                Bitmap bitmap = readerBitmap.getBitmap();
+                long start = System.currentTimeMillis();
+                assertTrue(wrapper.drawPage(i, 0, 0, bitmap.getWidth(), bitmap.getHeight(), bitmap));
+                long end = System.currentTimeMillis();
+                Log.i(TAG, "Performance testing: " + path +  " page: " + i + " ts: " + (end - start));
+            }
+            readerBitmap.recycleBitmap();
+            assertTrue(wrapper.nativeCloseDocument());
+        }
+
         assertTrue(wrapper.nativeDestroyLibrary());
     }
 
