b00916897480bd57d47fa1d0706f8f9284d3b95d
Zeng Zhu <zengzhu@gmail.com>
2016-02-29 16:05:20 +0800

    remove adobe plugin.

--- android/kreader/jni/adobe/thirdparty/png/src/MachOPngInterfaceDebugPrefix.h
+++
@@ -1,8 +0,0 @@
-#include <MSLCarbonPrefix.h>
-#include "MacLibpngPrefix.h"
-#include "MacDebugDefs.h"
-#include "alloc_redirect.h"
-
-#ifdef __cplusplus
-	#include "DebugNew.h"
-#endif
--- android/kreader/jni/adobe/thirdparty/png/src/MachOPngInterfaceReleasePrefix.h
+++
@@ -1,7 +0,0 @@
-#include <MSLCarbonPrefix.h>
-#include "MacLibpngPrefix.h"
-#include "MacReleaseDefs.h"
-#include "alloc_redirect.h"
-#ifdef __cplusplus
-	#include "DebugNew.h"
-#endif
