aa2710d1b5813cd8065cc70dd474814594a8fa08
Zeng Zhu <zengzhu@gmail.com>
2016-02-29 16:08:06 +0800

    add file nio.

--- android/kreader/src/com/onyx/kreader/formats/model/BookReaderContext.java
+++ android/kreader/src/com/onyx/kreader/formats/model/BookReaderContext.java
@@ -1,16 +1,16 @@
 package com.onyx.kreader.formats.model;
 
-import com.onyx.kreader.formats.filesystem.BookNIO;
+import com.onyx.kreader.formats.filesystem.FileNIO;
 
 /**
  * Created by zengzhu on 2/28/16.
  */
 public class BookReaderContext {
 
-    public BookNIO bookStream;
+    public FileNIO bookStream;
 
     public BookReaderContext(final String path) {
-        bookStream = new BookNIO(path);
+        bookStream = new FileNIO(path);
     }
 
 
