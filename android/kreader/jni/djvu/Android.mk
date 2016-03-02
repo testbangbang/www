b41e7d2e3a34c6126057ec8a2eb9bb8a85b6d057
Joy <joy@onyx-international.com>
2016-03-02 13:21:17 +0800

    import djvu jni code from onyx_reader

---
+++ android/kreader/jni/djvu/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+TOP_LOCAL_PATH := $(LOCAL_PATH)
+
+include $(TOP_LOCAL_PATH)/Core.mk
+
+include $(CLEAR_VARS)
+LOCAL_ARM_MODE := arm
+
+LOCAL_C_INCLUDES := \
+	$(TOP_LOCAL_PATH) \
+	$(TOP_LOCAL_PATH)/libdjvu 
+
+LOCAL_MODULE    := djvu
+LOCAL_SRC_FILES := djvu.c \
+		    orion_bitmap.c
+		    
+LOCAL_STATIC_LIBRARIES := djvucore
+
+LOCAL_LDLIBS    := -lm -llog -ljnigraphics
+
+include $(BUILD_SHARED_LIBRARY)
