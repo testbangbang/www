c3fb41f05eba5470b7e9c25f759792b8e23bd7c6
Zeng Zhu <zengzhu@gmail.com>
2016-03-11 18:04:32 +0800

    add text model position.

---
+++ android/kreader/src/com/onyx/kreader/formats/model/TextModelPosition.java
@@ -0,0 +1,23 @@
+package com.onyx.kreader.formats.model;
+
+/**
+ * Created by zengzhu on 3/11/16.
+ */
+public class TextModelPosition {
+
+
+    private int currentParagraph;
+    private int paragraphCount;
+
+    private int currentEntry;
+    private int entryCount;
+
+    private int currentOffset;
+    private int length;
+
+    public TextModelPosition() {
+
+    }
+
+
+}
