df6c06cdf1744ee0cfb99051612d7ae3c0b4d23a
Joy <joycodex@hotmail.com>
2016-05-17 21:07:10 +0800

    add epdcontroller which is obfuscated by proguard

---
+++ android/kreader/libraries/epdcontroller/src/main/java/com/onyx/android/sdk/data/RefValue.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.onyx.android.sdk.data;
+
+/**
+ * @author joy
+ *
+ */
+public class RefValue<T>
+{
+    private T mValue = null;
+    
+    public RefValue()
+    {
+    }
+    
+    public RefValue(T v)
+    {
+        mValue = v;
+    }
+    
+    public T getValue()
+    {
+        return mValue;
+    }
+    public void setValue(T v)
+    {
+        mValue = v;
+    }
+}
