615fd7f2fcb288aeb2b4c6c814fb13578aece34a
Joy <joycodex@hotmail.com>
2016-05-24 19:38:37 +0800

    update ScreenOrientationChangedAction, use ChangeViewConfigRequest instead of CreateViewRequest

--- android/kreader/app/src/main/java/com/onyx/kreader/ui/actions/ScreenOrientationChangedAction.java
+++ android/kreader/app/src/main/java/com/onyx/kreader/ui/actions/ScreenOrientationChangedAction.java
@@ -2,7 +2,7 @@ package com.onyx.kreader.ui.actions;
 
 import com.onyx.kreader.common.BaseCallback;
 import com.onyx.kreader.common.BaseRequest;
-import com.onyx.kreader.host.request.CreateViewRequest;
+import com.onyx.kreader.host.request.ChangeViewConfigRequest;
 import com.onyx.kreader.ui.ReaderActivity;
 
 /**
@@ -11,7 +11,7 @@ import com.onyx.kreader.ui.ReaderActivity;
 public class ScreenOrientationChangedAction extends BaseAction {
     @Override
     public void execute(final ReaderActivity readerActivity) {
-        BaseRequest config = new CreateViewRequest(readerActivity.getDisplayWidth(), readerActivity.getDisplayHeight());
+        BaseRequest config = new ChangeViewConfigRequest(readerActivity.getDisplayWidth(), readerActivity.getDisplayHeight());
         readerActivity.getReader().submitRequest(readerActivity, config, new BaseCallback() {
             @Override
             public void done(BaseRequest request, Exception e) {
