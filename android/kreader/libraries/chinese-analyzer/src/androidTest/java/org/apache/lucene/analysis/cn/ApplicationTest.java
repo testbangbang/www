48f3fede8a2f0843cc3f3d5b40e471a2999b8ce6
Joy <joy@onyx-international.com>
2016-06-24 17:51:59 +0800

    initial import chinese-analyzer

---
+++ android/kreader/libraries/chinese-analyzer/src/androidTest/java/org/apache/lucene/analysis/cn/ApplicationTest.java
@@ -0,0 +1,15 @@
+package org.apache.lucene.analysis.cn;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    private static String TAG = ApplicationTest.class.getSimpleName();
+
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
