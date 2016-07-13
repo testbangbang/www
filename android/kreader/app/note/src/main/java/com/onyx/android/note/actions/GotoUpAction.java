020a50a182be9e5328d1da2e2cce512dd4340125
Solskjaer49 <solskjaer49@gmail.com>
2016-07-13 19:28:44 +0800

    Initial Adjust Note UI as MX request. 1.add pen width status show. 2.add date show in manageActivity. 3.add bg shadow as req(need new 9.png,effect is not good in note app) 4.change ic as mx req. 5.change btn pos as mx req.

--- android/kreader/app/note/src/main/java/com/onyx/android/note/actions/GotoUpAction.java
+++ android/kreader/app/note/src/main/java/com/onyx/android/note/actions/GotoUpAction.java
@@ -5,7 +5,6 @@ import com.onyx.android.sdk.common.request.BaseCallback;
 import com.onyx.android.sdk.common.request.BaseRequest;
 import com.onyx.android.sdk.scribble.data.NoteDataProvider;
 import com.onyx.android.sdk.scribble.data.NoteModel;
-import com.onyx.android.sdk.scribble.request.BaseNoteRequest;
 import com.onyx.android.sdk.scribble.request.note.NoteLibraryLoadRequest;
 
 /**
@@ -22,11 +21,14 @@ public class GotoUpAction<T extends ManageActivity>  extends BaseNoteAction<T> {
     @Override
     public void execute(final T activity, final BaseCallback callback) {
         final NoteModel noteModel = NoteDataProvider.load(activity, uniqueId);
+        final NoteModel parentModel = NoteDataProvider.load(activity,noteModel.getParentUniqueId());
+
         final NoteLibraryLoadRequest loadRequest = new NoteLibraryLoadRequest(noteModel.getParentUniqueId());
         activity.getNoteViewHelper().submit(activity, loadRequest, new BaseCallback() {
             @Override
             public void done(BaseRequest request, Throwable e) {
                 activity.setCurrentLibraryId(noteModel.getParentUniqueId());
+                activity.setCurrentLibraryName(parentModel == null ? "" : parentModel.getTitle());
                 activity.updateWithNoteList(loadRequest.getNoteList());
             }
         });
