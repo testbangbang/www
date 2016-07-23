c9a6f682af02a08ff88670c1739dc8bf3a3ae524
john <zeng.zhu@gmail.com>
2016-07-23 17:37:27 +0800

    add 3rd party stroke rendering lib.

--- android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/shape/NormalPencilShape.java
+++ android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/shape/NormalPencilShape.java
@@ -4,6 +4,7 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
+import com.hanvon.core.HWColorPaint;
 import com.onyx.android.sdk.scribble.EPDRenderer;
 import com.onyx.android.sdk.scribble.data.TouchPoint;
 import com.onyx.android.sdk.scribble.utils.ShapeUtils;
@@ -13,6 +14,8 @@ import com.onyx.android.sdk.scribble.utils.ShapeUtils;
  * One stroke
  */
 public class NormalPencilShape extends EPDShape {
+
+    private static boolean useHwColorPaint = false;
 
     public void onDown(final TouchPoint normalizedPoint, final TouchPoint screenPoint) {
         super.onDown(normalizedPoint, screenPoint);
@@ -30,6 +33,14 @@ public class NormalPencilShape extends EPDShape {
     }
 
     public void render(final Canvas canvas, final Paint paint, final Matrix matrix) {
+        if (useHwColorPaint) {
+            renderByHWColorPaint();
+        } else {
+            renderByDefault(canvas, paint, matrix);
+        }
+    }
+
+    private void renderByDefault(final Canvas canvas, final Paint paint, final Matrix matrix) {
         applyStrokeStyle(paint);
         Path path = getDisplayPath();
         if (path == null) {
@@ -37,6 +48,18 @@ public class NormalPencilShape extends EPDShape {
             setDisplayPath(path);
         }
         canvas.drawPath(path, paint);
+    }
+
+    private void renderByHWColorPaint() {
+        HWColorPaint.setPen(0, 3, 6, 3, 12);
+        HWColorPaint.setSimulatePressure(true);
+
+        float[] points = new float[2048];
+        int[] rect = new int[4];
+        for(TouchPoint touchPoint : getNormalizedPoints().getPoints()) {
+            HWColorPaint.drawLineEx((int)touchPoint.x, (int)touchPoint.y, touchPoint.pressure / 1024f, rect, points);
+        }
+        HWColorPaint.drawLineEx(-1, -1, 0, rect, points);
     }
 
 }
