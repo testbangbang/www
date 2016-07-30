87ab84732412e73b4de9ced761ded7d927e246f2
john <zeng.zhu@gmail.com>
2016-07-30 11:22:50 +0800

    add config to start different home activity.

---
+++ android/kreader/app/note/src/main/java/com/onyx/android/note/activity/StartupActivity.java
@@ -0,0 +1,55 @@
+package com.onyx.android.note.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.onyx.android.note.R;
+import com.onyx.android.note.utils.NoteAppConfig;
+import com.onyx.android.sdk.scribble.utils.DeviceConfig;
+import com.onyx.android.sdk.utils.ActivityUtil;
+
+public class StartupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+        startHomeActivityByDeviceConfig(NoteAppConfig.sharedInstance(this));
+    }
+
+    private boolean startHomeActivityByDeviceConfig(final NoteAppConfig deviceConfig) {
+        Intent intent = ActivityUtil.createIntent(deviceConfig.getHomeActivityPackageName(), deviceConfig.getHomeActivityClassName());
+        if (ActivityUtil.startActivitySafely(this, intent)) {
+            finish();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_startup, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ android/kreader/app/note/src/main/java/com/onyx/android/note/utils/NoteAppConfig.java
@@ -0,0 +1,113 @@
+package com.onyx.android.note.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.preference.PreferenceActivity;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.alibaba.fastjson.JSONArray;
+import com.onyx.android.note.BuildConfig;
+import com.onyx.android.sdk.data.GObject;
+import com.onyx.android.sdk.utils.RawResourceUtil;
+
+/**
+ * Created with IntelliJ IDEA. User: zhuzeng Date: 5/18/14 Time: 5:21 PM To change this template use
+ * File | Settings | File Templates.
+ */
+public class NoteAppConfig {
+
+    static private String TAG = NoteAppConfig.class.getSimpleName();
+    private GObject backend;
+    private static NoteAppConfig globalInstance;
+
+    static public final String HOME_ACTIVITY_PKG_NAME = "home_activity_pkg";
+    static public final String HOME_ACTIVITY_CLS_NAME = "home_activity_cls";
+
+    static public final boolean useDebugConfig = false;
+
+    static public NoteAppConfig sharedInstance(Context context) {
+        if (globalInstance == null) {
+            globalInstance = new NoteAppConfig(context);
+        }
+        return globalInstance;
+    }
+
+    public final String getHomeActivityPackageName() {
+        if (backend.hasKey(HOME_ACTIVITY_PKG_NAME)) {
+            return backend.getString(HOME_ACTIVITY_PKG_NAME);
+        }
+        return null;
+    }
+
+    public final String getHomeActivityClassName() {
+        if (backend.hasKey(HOME_ACTIVITY_CLS_NAME)) {
+            return backend.getString(HOME_ACTIVITY_CLS_NAME);
+        }
+        return null;
+    }
+
+    private NoteAppConfig(Context context) {
+        backend = objectFromDebugModel(context);
+        if (backend != null) {
+            Log.i(TAG, "Using debug model.");
+            return;
+        }
+
+        backend = objectFromManufactureAndModel(context);
+        if (backend != null) {
+            Log.i(TAG, "Using manufacture model.");
+            return;
+        }
+
+        backend = objectFromModel(context);
+        if (backend != null) {
+            Log.i(TAG, "Using device model.");
+            return;
+        }
+
+        Log.i(TAG, "Using default model.");
+        backend = objectFromDefaultOnyxConfig(context);
+    }
+
+    private GObject objectFromManufactureAndModel(Context context) {
+        final String name = Build.MANUFACTURER + "_" + Build.MODEL;
+        return objectFromRawResource(context, name);
+    }
+
+    private GObject objectFromDebugModel(Context context) {
+        if (BuildConfig.DEBUG && useDebugConfig) {
+            return objectFromRawResource(context, "debug");
+        }
+        return null;
+    }
+
+    private GObject objectFromModel(Context context) {
+        final String name = Build.MODEL;
+        return objectFromRawResource(context, name);
+    }
+
+    private GObject objectFromDefaultOnyxConfig(Context context) {
+        return objectFromRawResource(context, "onyx");
+    }
+
+    private GObject objectFromRawResource(Context context, final String name) {
+        GObject object = null;
+        try {
+            int res = context.getResources().getIdentifier(name.toLowerCase(), "raw", context.getPackageName());
+            object = RawResourceUtil.objectFromRawResource(context, res);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return object;
+        }
+    }
+
+}
