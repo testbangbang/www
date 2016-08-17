7cdc4f98ae8e95c1eec4b0c632d91fa48443f481
Joy <joy@onyx-international.com>
2016-08-17 17:04:21 +0800

    kreader: refactoring Debug class

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/common/Debug.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/common/Debug.java
@@ -10,13 +10,25 @@ public class Debug {
     private static final String TAG = Debug.class.getSimpleName();
     private static boolean debug = true;
 
-    public static void d(String msg) {
-        d(TAG, msg);
+    public static void d(final String msg, final Object... args) {
+        d(TAG, msg, args);
     }
 
-    public static void d(String tag, String msg) {
+    public static void d(final String tag, final String msg, final Object... args) {
         if (BuildConfig.DEBUG && debug) {
-            Log.d(tag, msg);
+            Log.d(tag, formatString(msg, args));
         }
+    }
+
+    public static void e(final String msg, final Object... args) {
+        e(TAG, msg, args);
+    }
+
+    public static void e(final String tag, final String msg, Object... args) {
+        Log.e(tag, formatString(msg, args));
+    }
+
+    private static String formatString(String str, Object... args) {
+        return String.format(null, str, args);
     }
 }
