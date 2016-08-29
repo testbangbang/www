c6a4c17bdacfd4a5973fc613ff578e4bc90aef3b
Joy <joy@onyx-international.com>
2016-08-29 14:54:05 +0800

    kreader: clean up DeviceUtils.setFullScreen()

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/utils/DeviceUtils.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/utils/DeviceUtils.java
@@ -134,12 +134,16 @@ public class DeviceUtils {
 
     public static void setFullScreen(Activity activity, boolean fullScreen) {
         if (Build.VERSION.SDK_INT >= 19) {
-            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            if (fullScreen) {
+                activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+            } else {
+
+            }
             return;
         }
 
