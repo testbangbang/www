f4adfd69bea43d7a5244b3cb36cbb693f7fa611d
梁小明 <351062367@qq.com>
2016-08-29 10:52:23 +0800

    remove drawPage onresume

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/ReaderActivity.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/ReaderActivity.java
@@ -93,7 +93,6 @@ public class ReaderActivity extends ActionBarActivity {
     protected void onResume() {
         checkForNewConfiguration();
         super.onResume();
-        getReaderDataHolder().redrawPage(false);
     }
 
     @Override
@@ -315,7 +314,7 @@ public class ReaderActivity extends ActionBarActivity {
                     getReaderDataHolder().setDisplaySize(surfaceView.getWidth(), surfaceView.getHeight());
                     new ChangeViewConfigAction().execute(readerDataHolder);
                 } else {
-                    readerDataHolder.redrawPage(true);
+                    readerDataHolder.redrawPage();
                 }
             }
         });
