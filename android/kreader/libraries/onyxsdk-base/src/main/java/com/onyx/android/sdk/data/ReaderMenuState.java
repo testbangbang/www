27cb399185ccafd7d5d2ae77d2e998aa54d66259
梁小明 <351062367@qq.com>
2016-08-29 10:48:15 +0800

    change update updateBackwardForwardState way

--- android/kreader/libraries/onyxsdk-base/src/main/java/com/onyx/android/sdk/data/ReaderMenuState.java
+++ android/kreader/libraries/onyxsdk-base/src/main/java/com/onyx/android/sdk/data/ReaderMenuState.java
@@ -7,6 +7,8 @@ public class ReaderMenuState {
     private static final String TITLE_TAG = "title";
     private static final String PAGE_INDEX_TAG = "page_index";
     private static final String PAGE_COUNT_TAG = "page_count";
+    private static final String CAN_GO_BACK = "can_go_back";
+    private static final String CAN_GO_FORWARD = "can_go_forward";
 
     GObject data = new GObject();
 
@@ -32,6 +34,22 @@ public class ReaderMenuState {
 
     public void setPageCount(int count) {
         data.putInt(PAGE_COUNT_TAG, count);
+    }
+
+    public boolean canGoBack(){
+        return data.getBoolean(CAN_GO_BACK,false);
+    }
+
+    public void setCanGoBack(boolean canGoBack){
+        data.putBoolean(CAN_GO_BACK, canGoBack);
+    }
+
+    public boolean canGoForward(){
+        return data.getBoolean(CAN_GO_FORWARD,false);
+    }
+
+    public void setCanGoForward(boolean canGoForward){
+        data.putBoolean(CAN_GO_FORWARD, canGoForward);
     }
 
     public int getValue(String key, int defaultValue) {
