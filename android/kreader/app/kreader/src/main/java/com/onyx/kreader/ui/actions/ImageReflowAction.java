2f06328aede788a6063c1ae93f47c42ec13fdba2
梁小明 <351062367@qq.com>
2016-09-14 17:19:04 +0800

    fix reflow details

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/actions/ImageReflowAction.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/actions/ImageReflowAction.java
@@ -1,11 +1,15 @@
 package com.onyx.kreader.ui.actions;
 
+import com.onyx.android.sdk.common.request.BaseCallback;
+import com.onyx.android.sdk.common.request.BaseRequest;
 import com.onyx.android.sdk.data.PageConstants;
+import com.onyx.kreader.R;
 import com.onyx.kreader.common.BaseReaderRequest;
 import com.onyx.kreader.host.navigation.NavigationArgs;
 import com.onyx.kreader.host.request.ChangeLayoutRequest;
 import com.onyx.kreader.reflow.ImageReflowSettings;
 import com.onyx.kreader.ui.data.ReaderDataHolder;
+import com.onyx.kreader.ui.dialog.DialogLoading;
 import com.onyx.kreader.ui.dialog.DialogReflowSettings;
 
 /**
@@ -13,6 +17,7 @@ import com.onyx.kreader.ui.dialog.DialogReflowSettings;
  */
 public class ImageReflowAction extends BaseAction {
     private DialogReflowSettings reflowSettingsDialog;
+    private DialogLoading dialogLoading;
 
     public void execute(final ReaderDataHolder readerDataHolder) {
         showReflowSettingsDialog(readerDataHolder);
@@ -27,13 +32,20 @@ public class ImageReflowAction extends BaseAction {
                 @Override
                 public void onFinished(boolean confirm, ImageReflowSettings settings) {
                     if (confirm && settings != null) {
-                        BaseReaderRequest request = new ChangeLayoutRequest(PageConstants.IMAGE_REFLOW_PAGE, new NavigationArgs());
-                        readerDataHolder.submitRenderRequest(request);
+                        showLoadingDialog(readerDataHolder);
                         hideReflowSettingsDialog();
+                        BaseReaderRequest request = new ChangeLayoutRequest(PageConstants.IMAGE_REFLOW_PAGE, new NavigationArgs());
+                        readerDataHolder.submitRenderRequest(request, new BaseCallback() {
+                            @Override
+                            public void done(BaseRequest request, Throwable e) {
+                                hideLoadingDialog();
+                            }
+                        });
                     }
                 }
             });
             reflowSettingsDialog.show();
+            ShowReaderMenuAction.hideReaderMenu();
         }
     }
 
@@ -41,6 +53,27 @@ public class ImageReflowAction extends BaseAction {
         if (reflowSettingsDialog != null) {
             reflowSettingsDialog.dismiss();
             reflowSettingsDialog = null;
+        }
+    }
+
+    private DialogLoading showLoadingDialog(final ReaderDataHolder holder) {
+        if (dialogLoading == null) {
+            dialogLoading = new DialogLoading(reflowSettingsDialog.getContext(),
+                    holder.getContext().getResources().getString(R.string.reflowing),
+                    true, new DialogLoading.Callback() {
+                @Override
+                public void onCanceled() {
+                }
+            });
+        }
+        dialogLoading.show();
+        return dialogLoading;
+    }
+
+    private void hideLoadingDialog() {
+        if (dialogLoading != null) {
+            dialogLoading.dismiss();
+            dialogLoading = null;
         }
     }
 }
