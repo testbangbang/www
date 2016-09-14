e28d92740227b9aa591de3491a98b18327eecf57
梁小明 <351062367@qq.com>
2016-09-14 19:08:45 +0800

    baseaction add loadingdialog

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/actions/BaseAction.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/actions/BaseAction.java
@@ -3,12 +3,39 @@ package com.onyx.kreader.ui.actions;
 
 import com.onyx.android.sdk.common.request.BaseCallback;
 import com.onyx.kreader.ui.data.ReaderDataHolder;
+import com.onyx.kreader.ui.dialog.DialogLoading;
 
 public abstract class BaseAction {
+
+    private DialogLoading dialogLoading;
 
     public abstract void execute(final ReaderDataHolder readerDataHolder);
 
     public void execute(final ReaderDataHolder readerDataHolder, BaseCallback baseCallback){
     }
 
+    private DialogLoading showLoadingDialog(final ReaderDataHolder holder, String title) {
+        if (dialogLoading == null) {
+            dialogLoading = new DialogLoading(holder.getContext(),
+                    title,
+                    true, new DialogLoading.Callback() {
+                @Override
+                public void onCanceled() {
+                }
+            });
+        }
+        dialogLoading.show();
+        return dialogLoading;
+    }
+
+    public DialogLoading showLoadingDialog(final ReaderDataHolder holder, int titleResId) {
+        return showLoadingDialog(holder, holder.getContext().getString(titleResId));
+    }
+
+    public void hideLoadingDialog() {
+        if (dialogLoading != null) {
+            dialogLoading.dismiss();
+            dialogLoading = null;
+        }
+    }
 }
