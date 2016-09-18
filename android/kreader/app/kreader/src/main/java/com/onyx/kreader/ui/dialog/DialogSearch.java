db68559c15b5f57959843a59a27073e68a9ec465
梁小明 <351062367@qq.com>
2016-09-18 14:19:12 +0800

    fix softkeyboard cause view move up

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/dialog/DialogSearch.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/dialog/DialogSearch.java
@@ -14,6 +14,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
@@ -88,6 +89,7 @@ public class DialogSearch extends Dialog{
         setContentView(R.layout.dialog_search);
         this.readerDataHolder = readerDataHolder;
         searchRows = getContext().getResources().getInteger(R.integer.search_row);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
         init();
     }
 
