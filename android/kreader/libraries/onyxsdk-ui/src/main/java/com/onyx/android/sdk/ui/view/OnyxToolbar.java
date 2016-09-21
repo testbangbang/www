4161b8422ba3d9ad11548ee13f2a21796638df9a
梁小明 <351062367@qq.com>
2016-09-21 17:18:53 +0800

    reformat onyxToolbar code

--- android/kreader/libraries/onyxsdk-ui/src/main/java/com/onyx/android/sdk/ui/view/OnyxToolbar.java
+++ android/kreader/libraries/onyxsdk-ui/src/main/java/com/onyx/android/sdk/ui/view/OnyxToolbar.java
@@ -12,14 +12,14 @@ import com.onyx.android.sdk.ui.R;
 import com.onyx.android.sdk.utils.DimenUtils;
 
 /**
- * 工具条
- * 线性布局
- * 可以插入SpaceView来调整布局
- * SpaceView可以通过Weight来设置占有的百分比
- * 填充方式为Average时会忽悠SpaceView
+ * toolbar
+ * Linear layout
+ * can insert SpaceView to adjust the layout
+ * SpaceView possession can be set by Weight percentage
+ * Will ignore SpaceView filling way in business average style
  * Created by ming on 16/9/20.
  */
-public class OnyxToolbar extends ViewGroup{
+public class OnyxToolbar extends ViewGroup {
 
     public interface OnMenuClickListener {
         OnyxToolbar OnClickListener(View view);
@@ -40,10 +40,15 @@ public class OnyxToolbar extends ViewGroup{
     int paddingTop;
     int paddingBottom;
 
-    //工具条出现的位置
-    public enum Direction{Left,Top,Right,Bottom}
-    //工具条内容的填充方式 WrapContent 根据内容填充  Average 平分
-    public enum FillStyle{WrapContent,Average}
+    //The position of the toolbar
+    public enum Direction {
+        Left, Top, Right, Bottom
+    }
+
+    //The toolbar content way of filling
+    public enum FillStyle {
+        WrapContent, Average
+    }
 
     public OnyxToolbar(Context context) {
         super(context);
@@ -64,7 +69,7 @@ public class OnyxToolbar extends ViewGroup{
         this.onMenuClickListener = onMenuClickListener;
     }
 
-    private void init(){
+    private void init() {
         dividerView = generateDefaultDividerView();
         addView(dividerView);
     }
@@ -87,24 +92,24 @@ public class OnyxToolbar extends ViewGroup{
         setupListener(child);
     }
 
-    private void setupListener(View view){
-        if (!isMenuView(view)){
+    private void setupListener(View view) {
+        if (!isMenuView(view)) {
             return;
         }
         view.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (onMenuClickListener != null){
+                if (onMenuClickListener != null) {
                     OnyxToolbar toolbar = onMenuClickListener.OnClickListener(v);
-                    if (currentExpandedView != v && toolbar != null){
-                        if (currentExpandedToolbar != null){
+                    if (currentExpandedView != v && toolbar != null) {
+                        if (currentExpandedToolbar != null) {
                             removeView(currentExpandedToolbar);
                         }
                         currentExpandedToolbar = toolbar;
                         currentExpandedToolbar.setDirection(direction);
                         addView(currentExpandedToolbar);
                         currentExpandedView = v;
-                    }else {
+                    } else {
                         removeView(currentExpandedToolbar);
                         currentExpandedView = null;
                         currentExpandedToolbar = null;
@@ -115,7 +120,7 @@ public class OnyxToolbar extends ViewGroup{
         });
     }
 
-    private View generateDefaultDividerView(){
+    private View generateDefaultDividerView() {
         View view = new View(getContext());
         view.setBackgroundColor(getResources().getColor(android.R.color.black));
         view.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, DimenUtils.dip2px(getContext(), 1)));
@@ -135,16 +140,16 @@ public class OnyxToolbar extends ViewGroup{
             this.measureChild(childView, widthMeasureSpec, heightMeasureSpec);
             LayoutParams lp = (LayoutParams) childView.getLayoutParams();
             totalWeight += lp.weight;
-            if (isMenuView(childView)){
+            if (isMenuView(childView)) {
                 menuViewTotalWidth += childView.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
                 maxHeight = Math.max(childView.getMeasuredHeight() + lp.topMargin + lp.bottomMargin, maxHeight);
-                menuViewCount ++;
+                menuViewCount++;
             }
 
-            if (childView == dividerView){
+            if (childView == dividerView) {
                 maxHeight += dividerView.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
             }
-            if (childView == currentExpandedToolbar){
+            if (childView == currentExpandedToolbar) {
                 maxHeight += currentExpandedToolbar.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
             }
         }
@@ -166,17 +171,17 @@ public class OnyxToolbar extends ViewGroup{
 
             int measureHeight = childView.getMeasuredHeight();
             int measuredWidth = childView.getMeasuredWidth();
-            LayoutParams lp = (LayoutParams)childView.getLayoutParams();
+            LayoutParams lp = (LayoutParams) childView.getLayoutParams();
 
-            if (childView instanceof SpaceView){
+            if (childView instanceof SpaceView) {
                 measuredWidth = calculateSpaceViewWidth(lp.weight, menuViewTotalWidth);
             }
 
-            switch (direction){
+            switch (direction) {
                 case Left:
                     break;
                 case Top:
-                    totalWidth = layoutChildTop(childView, totalWidth,  measuredWidth, measureHeight);
+                    totalWidth = layoutChildTop(childView, totalWidth, measuredWidth, measureHeight);
                     break;
                 case Right:
                     break;
@@ -188,40 +193,41 @@ public class OnyxToolbar extends ViewGroup{
     }
 
     /**
-     * 底部布局
-     * currentExpandedToolbar 展开的工具条在顶部，Top位置在顶部
-     * dividerView 分割View 总高度减去展开的工具条的高度得出他的Top位置
-     * menuView 菜单项 总高度 减去 展开的工具条和分割View的高度 得出他的Top位置
-     * @param childView 子view
-     * @param totalWidth menuView的总长度
-     * @param measuredWidth 子view的宽度
-     * @param measureHeight 子view的高度
+     * At the bottom of the layout
+     * currentExpandedToolbar: In the toolbar at the Top, Top position at the Top
+     * dividerView: Segmentation is always highly minus the View of the height of the bar he come to the Top position
+     * menuView: Menu item total height minus in the bar and the height of the split View his Top position
+     *
+     * @param childView     childView
+     * @param totalWidth    The total length of menuView
+     * @param measuredWidth The width of the child view
+     * @param measureHeight The height of the child view
      * @return
      */
-    private int layoutChildBottom(View childView, int totalWidth, int measuredWidth, int measureHeight){
-        LayoutParams lp = (LayoutParams)childView.getLayoutParams();
+    private int layoutChildBottom(View childView, int totalWidth, int measuredWidth, int measureHeight) {
+        LayoutParams lp = (LayoutParams) childView.getLayoutParams();
 
-        if (childView == currentExpandedToolbar){
+        if (childView == currentExpandedToolbar) {
             childView.layout(lp.leftMargin
-                    ,lp.topMargin
-                    , getMeasuredWidth()  - lp.rightMargin
+                    , lp.topMargin
+                    , getMeasuredWidth() - lp.rightMargin
                     , measureHeight + lp.topMargin);
-        }else if (childView == dividerView){
+        } else if (childView == dividerView) {
             int expandedToolbarHeight = getHeightWidthMargin(currentExpandedToolbar);
             childView.layout(paddingLeft + lp.leftMargin
                     , expandedToolbarHeight + lp.topMargin + paddingTop
                     , getMeasuredWidth() - paddingRight - lp.rightMargin
                     , expandedToolbarHeight + measureHeight + lp.topMargin + paddingTop);
-        }else {
+        } else {
             int height = getHeightWidthMargin(currentExpandedToolbar) + getHeightWidthMargin(dividerView);
             int startLeft = totalWidth + lp.leftMargin;
             int startTop = lp.topMargin + height + paddingTop;
 
-            if (!isSpaceView(childView) && fillStyle == FillStyle.Average){
+            if (!isSpaceView(childView) && fillStyle == FillStyle.Average) {
                 startLeft = getChildStartWhenAverage(menuViewIndex, childView);
             }
 
-            if (!isSpaceView(childView)){
+            if (!isSpaceView(childView)) {
                 menuViewIndex++;
             }
 
@@ -235,39 +241,40 @@ public class OnyxToolbar extends ViewGroup{
     }
 
     /**
-     * 顶部布局
-     * currentExpandedToolbar 展开的工具条在底部，总高度 减去 它自己的高度 得出他的Top位置
-     * dividerView 分割View 总高度减去展开的工具条的高度得出他的Top位置
-     * menuView 菜单项在顶部，Top位置在顶部
-     * @param childView 子view
-     * @param totalWidth menuView的总长度
-     * @param measuredWidth 子view的宽度
-     * @param measureHeight 子view的高度
+     * At the top of the layout
+     * currentExpandedToolbar: In the toolbar at the bottom, total height minus its own highly he come to the Top position
+     * dividerView: Segmentation is always highly minus the View of the height of the bar he come to the Top position
+     * menuView: A menu item at the Top, the Top position at the Top
+     *
+     * @param childView     childView
+     * @param totalWidth    The total length of menuView
+     * @param measuredWidth The width of the child view
+     * @param measureHeight The height of the child view
      * @return
      */
-    private int layoutChildTop(View childView, int totalWidth, int measuredWidth, int measureHeight){
-        LayoutParams lp = (LayoutParams)childView.getLayoutParams();
+    private int layoutChildTop(View childView, int totalWidth, int measuredWidth, int measureHeight) {
+        LayoutParams lp = (LayoutParams) childView.getLayoutParams();
 
-        if (childView == currentExpandedToolbar){
+        if (childView == currentExpandedToolbar) {
             childView.layout(lp.leftMargin
-                    ,getMeasuredHeight()  - lp.topMargin - measureHeight
+                    , getMeasuredHeight() - lp.topMargin - measureHeight
                     , getMeasuredWidth() - lp.rightMargin
                     , getMeasuredHeight() - lp.bottomMargin);
-        }else if (childView == dividerView){
+        } else if (childView == dividerView) {
             int expandedToolbarHeight = currentExpandedToolbar == null ? 0 : currentExpandedToolbar.getMeasuredHeight();
             childView.layout(paddingLeft + lp.leftMargin
-                    ,getMeasuredHeight() - paddingBottom - lp.bottomMargin - measureHeight - expandedToolbarHeight
+                    , getMeasuredHeight() - paddingBottom - lp.bottomMargin - measureHeight - expandedToolbarHeight
                     , getMeasuredWidth() - paddingRight - lp.rightMargin
                     , getMeasuredHeight() - paddingBottom - lp.bottomMargin - expandedToolbarHeight);
-        }else {
+        } else {
             int startLeft = totalWidth + lp.leftMargin;
             int startTop = lp.topMargin + paddingTop;
 
-            if (!isSpaceView(childView) && fillStyle == FillStyle.Average){
+            if (!isSpaceView(childView) && fillStyle == FillStyle.Average) {
                 startLeft = getChildStartWhenAverage(menuViewIndex, childView);
             }
 
-            if (!isSpaceView(childView)){
+            if (!isSpaceView(childView)) {
                 menuViewIndex++;
             }
 
@@ -280,31 +287,31 @@ public class OnyxToolbar extends ViewGroup{
         return totalWidth;
     }
 
-    private int getChildStartWhenAverage(int index, View childView){
-        int averageWidth = getMeasuredWidth()/menuViewCount;
-        int centerPointX = averageWidth/2 + index * averageWidth;
-        return centerPointX - childView.getMeasuredWidth()/2;
+    private int getChildStartWhenAverage(int index, View childView) {
+        int averageWidth = getMeasuredWidth() / menuViewCount;
+        int centerPointX = averageWidth / 2 + index * averageWidth;
+        return centerPointX - childView.getMeasuredWidth() / 2;
     }
 
-    private int getHeightWidthMargin(View view){
+    private int getHeightWidthMargin(View view) {
         int height = 0;
-        if (view != null){
-            LayoutParams lp = (LayoutParams)view.getLayoutParams();
+        if (view != null) {
+            LayoutParams lp = (LayoutParams) view.getLayoutParams();
             height = view.getMeasuredHeight() + lp.bottomMargin + lp.topMargin;
         }
         return height;
     }
 
 
-    private boolean isMenuView(View view){
+    private boolean isMenuView(View view) {
         return !(view instanceof SpaceView) && view != dividerView && view != currentExpandedToolbar;
     }
 
-    private boolean isSpaceView(View view){
+    private boolean isSpaceView(View view) {
         return view instanceof SpaceView;
     }
 
-    private int calculateSpaceViewWidth(float weight, int totalWidth){
+    private int calculateSpaceViewWidth(float weight, int totalWidth) {
         int spaceTotalWidth = getMeasuredWidth() - totalWidth - getPaddingLeft() - getPaddingRight();
         return (int) (spaceTotalWidth * weight / totalWeight);
     }
@@ -315,8 +322,7 @@ public class OnyxToolbar extends ViewGroup{
 
         public LayoutParams(Context arg0, AttributeSet arg1) {
             super(arg0, arg1);
-            TypedArray a =
-                    arg0.obtainStyledAttributes(arg1, R.styleable.OnyxToolbar);
+            TypedArray a = arg0.obtainStyledAttributes(arg1, R.styleable.OnyxToolbar);
             weight = a.getFloat(R.styleable.OnyxToolbar_weight, 0);
         }
 
@@ -351,22 +357,22 @@ public class OnyxToolbar extends ViewGroup{
         return p instanceof LayoutParams;
     }
 
-    public static class Builder{
+    public static class Builder {
 
-        public static ImageView createImageView(Context content, int resId){
+        public static ImageView createImageView(Context content, int resId) {
             ImageView image = new ImageView(content);
             image.setImageResource(resId);
             setDefaultLayoutParams(content, image);
             return image;
         }
 
-        public static TextView createTextView(Context context){
+        public static TextView createTextView(Context context) {
             TextView textView = new TextView(context);
             setDefaultLayoutParams(context, textView);
             return textView;
         }
 
-        public static SpaceView createSpaceView(Context context, float weight){
+        public static SpaceView createSpaceView(Context context, float weight) {
             SpaceView spaceView = new SpaceView(context);
             OnyxToolbar.LayoutParams lp = new OnyxToolbar.LayoutParams(0, 1);
             lp.weight = weight;
@@ -374,7 +380,7 @@ public class OnyxToolbar extends ViewGroup{
             return spaceView;
         }
 
-        public static void setDefaultLayoutParams(Context content, View view){
+        public static void setDefaultLayoutParams(Context content, View view) {
             int margin = DimenUtils.dip2px(content, 5);
             view.setBackgroundResource(R.drawable.imagebtn_bg);
             OnyxToolbar.LayoutParams lp = new OnyxToolbar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
