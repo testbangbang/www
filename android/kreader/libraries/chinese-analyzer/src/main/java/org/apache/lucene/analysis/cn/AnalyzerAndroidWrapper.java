d993199a457e8c332d6e251265cea971390b5f00
Joy <joycodex@hotmail.com>
2016-10-11 20:04:22 +0800

    chinese-analyzer: cache stopWords to avoid loading each time

--- android/kreader/libraries/chinese-analyzer/src/main/java/org/apache/lucene/analysis/cn/AnalyzerAndroidWrapper.java
+++ android/kreader/libraries/chinese-analyzer/src/main/java/org/apache/lucene/analysis/cn/AnalyzerAndroidWrapper.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.concurrent.RunnableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -24,6 +25,8 @@ public class AnalyzerAndroidWrapper {
     private static Context context;
     private static AtomicBoolean initialized = new AtomicBoolean(false);
     private static AtomicBoolean initializing = new AtomicBoolean(false);
+
+    private static Set<String> stopWords = null;
 
     public static void initialize(final Context context, boolean background) {
         if (isInitialized()) {
@@ -84,7 +87,14 @@ public class AnalyzerAndroidWrapper {
         }
         try {
             Token nt = new Token();
-            Analyzer ca = new SmartChineseAnalyzer(true);
+            if (stopWords == null) {
+                try {
+                    stopWords = SmartChineseAnalyzer.loadStopWords(openAssetFile("stopwords.txt"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Analyzer ca = new SmartChineseAnalyzer(stopWords);
             TokenStream ts = ca.tokenStream("sentence", new StringReader(sentence));
             nt = ts.next(nt);
             while (nt != null) {
