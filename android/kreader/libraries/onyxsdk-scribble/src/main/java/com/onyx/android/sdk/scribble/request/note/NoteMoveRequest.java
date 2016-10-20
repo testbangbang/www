734882fedd4feaa0dcd0fbd57c3864859cc60491
Solskjaer49 <solskjaer49@gmail.com>
2016-10-20 18:04:47 +0800

    Add Move Fold/File name regality check as MX request.

--- android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/request/note/NoteMoveRequest.java
+++ android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/request/note/NoteMoveRequest.java
@@ -2,6 +2,7 @@ package com.onyx.android.sdk.scribble.request.note;
 
 import com.onyx.android.sdk.scribble.NoteViewHelper;
 import com.onyx.android.sdk.scribble.data.NoteDataProvider;
+import com.onyx.android.sdk.scribble.data.NoteModel;
 import com.onyx.android.sdk.scribble.request.BaseNoteRequest;
 
 import java.util.List;
@@ -10,19 +11,35 @@ import java.util.List;
  * Created by zhuzeng on 6/22/16.
  */
 public class NoteMoveRequest extends BaseNoteRequest {
-    String parentID;
-    List<String> uniqueIdList;
+    private String parentID;
+    private List<String> uniqueIdList;
+    private boolean checkNameLegality;
+    private boolean checkThisLevelOnly;
+    private boolean distinguishFileType;
 
-    public NoteMoveRequest(String parentID, List<String> uniqueIdList) {
+    public NoteMoveRequest(String parentID, List<String> uniqueIdList,
+                           boolean checkNameLegality, boolean checkThisLevelOnly, boolean distinguishFileType) {
         this.parentID = parentID;
         this.uniqueIdList = uniqueIdList;
+        this.checkNameLegality = checkNameLegality;
+        this.checkThisLevelOnly = checkThisLevelOnly;
+        this.distinguishFileType = distinguishFileType;
         setPauseInputProcessor(true);
         setResumeInputProcessor(false);
     }
 
     public void execute(final NoteViewHelper shapeManager) throws Exception {
         for (String uniqueId : uniqueIdList) {
-            NoteDataProvider.moveNote(getContext(), uniqueId, parentID);
+            if (checkNameLegality) {
+                NoteModel noteModel = NoteDataProvider.load(getContext(), uniqueId);
+                if (NoteDataProvider.checkNoteNameLegality(noteModel.getTitle(), parentID,
+                        noteModel.getType(), checkThisLevelOnly, distinguishFileType)) {
+                    NoteDataProvider.moveNote(getContext(), uniqueId, parentID);
+                }
+            } else {
+                NoteDataProvider.moveNote(getContext(), uniqueId, parentID);
+            }
+
         }
     }
 
