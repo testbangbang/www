39ee3bbada24ec5d0516e9e275fc404ce6b44db1
梁小明 <351062367@qq.com>
2016-10-22 16:27:38 +0800

    set HIT_TEST_SCALE_VALUE 1

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/highlight/HighlightCursor.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/highlight/HighlightCursor.java
@@ -23,7 +23,7 @@ public class HighlightCursor {
 
     public static final int BEGIN_CURSOR_INDEX = 0;
     public static final int END_CURSOR_INDEX   = 1;
-    public static final int HIT_TEST_SCALE_VALUE = 2;
+    public static final int HIT_TEST_SCALE_VALUE = 1;
 
     private Bitmap startCursorBitmap;
     private Bitmap endCursorBitmap;
