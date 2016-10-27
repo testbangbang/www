c4fab67f3b5b6fd83a3550a21bf423fb4be3ae58
梁小明 <351062367@qq.com>
2016-10-27 15:17:40 +0800

    load toc data when  goto next chapter

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/dialog/DialogQuickPreview.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/dialog/DialogQuickPreview.java
@@ -199,7 +199,7 @@ public class DialogQuickPreview extends Dialog {
     private ReaderDataHolder readerDataHolder;
     private int currentPage;
     private Callback callback;
-    private List<Integer> tocChapterNodeList = new ArrayList<>();
+    private List<Integer> tocChapterNodeList;
 
     public DialogQuickPreview(@NonNull final ReaderDataHolder readerDataHolder, Callback callback) {
         super(readerDataHolder.getContext(), android.R.style.Theme_NoTitleBar);
@@ -302,14 +302,14 @@ public class DialogQuickPreview extends Dialog {
         chapterBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gotoChapterIndex(true);
+                prepareGotoChapterIndex(true);
             }
         });
 
         chapterForward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gotoChapterIndex(false);
+                prepareGotoChapterIndex(false);
             }
         });
 
@@ -377,25 +377,32 @@ public class DialogQuickPreview extends Dialog {
         return page / grid.getGridType();
     }
 
-    private void loadDocumentTableOfContent() {
-        GetTableOfContentAction.execute(readerDataHolder, new BaseCallback() {
-            @Override
-            public void done(BaseRequest request, Throwable e) {
-                BaseReaderRequest readerRequest = (BaseReaderRequest) request;
-                ReaderDocumentTableOfContent toc = readerRequest.getReaderUserDataInfo().getTableOfContent();
-                chapterBack.setEnabled(toc != null && toc.getRootEntry() != null);
-                chapterForward.setEnabled(toc != null && toc.getRootEntry() != null);
-                buildChapterNodeList(toc);
-                updateChapterButtonState();
-            }
-        });
-    }
-
     private GPaginator getPaginator() {
         return gridRecyclerView.getPaginator();
     }
 
+    private void prepareGotoChapterIndex(final boolean back) {
+        if (tocChapterNodeList == null) {
+            new GetTableOfContentAction().execute(readerDataHolder, new BaseCallback() {
+                @Override
+                public void done(BaseRequest request, Throwable e) {
+                    BaseReaderRequest readerRequest = (BaseReaderRequest) request;
+                    ReaderDocumentTableOfContent toc = readerRequest.getReaderUserDataInfo().getTableOfContent();
+                    chapterBack.setEnabled(toc != null && toc.getRootEntry() != null);
+                    chapterForward.setEnabled(toc != null && toc.getRootEntry() != null);
+                    buildChapterNodeList(toc);
+                    gotoChapterIndex(back);
+                }
+            });
+        }else {
+            gotoChapterIndex(back);
+        }
+    }
+
     private void gotoChapterIndex(boolean back) {
+        if (tocChapterNodeList.size() <= 0) {
+            return;
+        }
         int chapterPosition;
         if (back) {
             int pageBegin = getPaginator().getCurrentPageBegin();
@@ -406,11 +413,6 @@ public class DialogQuickPreview extends Dialog {
         }
 
         gridRecyclerView.gotoPage(getGridPage(chapterPosition));
-    }
-
-    private void updateChapterButtonState() {
-        chapterBack.setEnabled(getPaginator().canPrevPage() && tocChapterNodeList.size() > 0);
-        chapterForward.setEnabled(getPaginator().canNextPage() && tocChapterNodeList.size() > 0);
     }
 
     private int getChapterPositionByPage(int page, boolean back) {
@@ -444,6 +446,7 @@ public class DialogQuickPreview extends Dialog {
     }
 
     private void buildChapterNodeList(ReaderDocumentTableOfContent toc) {
+        tocChapterNodeList = new ArrayList<>();
         ReaderDocumentTableOfContentEntry rootEntry = toc.getRootEntry();
         if (rootEntry.getChildren() != null) {
             buildChapterNode(rootEntry.getChildren());
@@ -479,7 +482,6 @@ public class DialogQuickPreview extends Dialog {
         }
         onPressedImageView(grid.getGridType());
         initPageProgress();
-        loadDocumentTableOfContent();
     }
 
     /**
@@ -500,7 +502,6 @@ public class DialogQuickPreview extends Dialog {
         previewMap.clear();
         adapter.requestMissingBitmaps();
         updatePageProgress();
-        updateChapterButtonState();
     }
 
     private void initPageProgress() {
