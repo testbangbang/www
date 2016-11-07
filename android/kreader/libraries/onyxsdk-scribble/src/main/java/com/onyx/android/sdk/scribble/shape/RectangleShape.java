e8428cb98e8fb9f06c91b2215663996140b2f4c5
john <zeng.zhu@gmail.com>
2016-11-07 16:15:04 +0800

    add hit test for rect.

--- android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/shape/RectangleShape.java
+++ android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/shape/RectangleShape.java
@@ -5,6 +5,8 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
 
+import com.onyx.android.sdk.scribble.utils.ShapeUtils;
+
 /**
  * Created by zhuzeng on 4/25/16.
  */
@@ -17,6 +19,14 @@ public class RectangleShape extends BaseShape {
 
     public boolean addMovePoint() {
         return false;
+    }
+
+    public boolean hitTest(final float x, final float y, final float radius) {
+        return ShapeUtils.hitTestLine(getDownPoint().x, getDownPoint().y, getDownPoint().x, getCurrentPoint().y, x, y, radius) ||
+                ShapeUtils.hitTestLine(getDownPoint().x, getDownPoint().y, getCurrentPoint().x, getDownPoint().y, x, y, radius) ||
+                ShapeUtils.hitTestLine(getDownPoint().x, getCurrentPoint().y, getCurrentPoint().x, getCurrentPoint().y, x, y, radius) ||
+                ShapeUtils.hitTestLine(getCurrentPoint().x, getDownPoint().y, getCurrentPoint().x, getCurrentPoint().y, x, y, radius);
+
     }
 
     public void render(final RenderContext renderContext) {
