2831238b1b62ab9375a1b4d3552b969e2bc916ad
Joy <joycodex@hotmail.com>
2016-11-08 20:46:54 +0800

    make hash_code_from_git.sh and version_code_from_git.sh more robust

--- android/kreader/version_code_from_git.sh
+++ android/kreader/version_code_from_git.sh
@@ -1,4 +1,22 @@
-cd $1 > /dev/null
-VERSION=`git rev-list --count --all .`
-cd - > /dev/null
-echo $VERSION
+#!/bin/bash
+
+if [ ! -n "$1" ]; then
+    VERSION=`git rev-list --count --all .`
+    echo $VERSION
+else
+    if [ -d "$1" ]; then
+        cd $1 > /dev/null
+        VERSION=`git rev-list --count --all .`
+    else
+        if [ -f "$1" ]; then
+            VERSION=`git rev-list --count --all $1`
+        fi
+    fi
+
+
+    if [ -d "$1" ]; then
+        cd - > /dev/null
+    fi
+
+    echo $VERSION
+fi
