ad4c030c848ced94fc20812af3a3dfbb98d06b19
wang.suicheng <wsc@onyx-international.com>
2016-11-29 12:26:50 +0800

    init eschool second and part third ui and func

--- android/kreader/app/eschool/src/main/java/com/onyx/android/eschool/SchoolApp.java
+++ android/kreader/app/eschool/src/main/java/com/onyx/android/eschool/SchoolApp.java
@@ -2,9 +2,53 @@ package com.onyx.android.eschool;
 
 import android.app.Application;
 
+import com.onyx.android.eschool.utils.StudentPreferenceManager;
+import com.onyx.android.sdk.data.CloudStore;
+
 /**
  * Created by zhuzeng on 14/11/2016.
  */
-
 public class SchoolApp extends Application {
+
+    static private SchoolApp sInstance = null;
+    static private CloudStore cloudStore = new CloudStore();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            sInstance = this;
+            StudentPreferenceManager.init(this);
+            initCloudStoreConfig();
+        } catch (Exception e) {
+            if (BuildConfig.DEBUG) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void initCloudStoreConfig() {
+        initCloudDatabase();
+        initCloudFileDownloader();
+    }
+
+    public void initCloudDatabase() {
+        CloudStore.initDatabase(this);
+    }
+
+    public void initCloudFileDownloader() {
+        CloudStore.initFileDownloader(this);
+    }
+
+    public void terminateCloudDatabase() {
+        CloudStore.terminateCloudDatabase();
+    }
+
+    public static SchoolApp singleton() {
+        return sInstance;
+    }
+
+    static public CloudStore getCloudStore() {
+        return cloudStore;
+    }
 }
