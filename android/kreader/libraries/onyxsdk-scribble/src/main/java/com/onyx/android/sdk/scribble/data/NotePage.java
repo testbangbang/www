ebece435d4268e655d613cd6abe0f527f2d19067
john <zeng.zhu@gmail.com>
2016-11-30 19:35:50 +0800

    clean up.

--- android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/data/NotePage.java
+++ android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/data/NotePage.java
@@ -1,9 +1,7 @@
 package com.onyx.android.sdk.scribble.data;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.Paint;
+
 import com.onyx.android.sdk.scribble.shape.*;
 
 import java.util.ArrayList;
