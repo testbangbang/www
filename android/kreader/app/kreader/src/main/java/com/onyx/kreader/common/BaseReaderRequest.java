0253a8c409cad616c52fcd92323c780324b9664b
Joy <joycodex@hotmail.com>
2016-12-02 16:30:27 +0800

    kreader: update page annotation rectangles for flow documents

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/common/BaseReaderRequest.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/common/BaseReaderRequest.java
@@ -3,12 +3,17 @@ package com.onyx.kreader.common;
 import android.util.Log;
 import com.onyx.android.sdk.common.request.BaseCallback;
 import com.onyx.android.sdk.common.request.BaseRequest;
+import com.onyx.android.sdk.data.PageInfo;
 import com.onyx.android.sdk.data.provider.DataProviderManager;
 import com.onyx.kreader.api.ReaderException;
+import com.onyx.kreader.api.ReaderHitTestManager;
+import com.onyx.kreader.api.ReaderSelection;
 import com.onyx.kreader.cache.ReaderBitmapImpl;
 import com.onyx.kreader.dataprovider.LegacySdkDataUtils;
 import com.onyx.kreader.host.wrapper.Reader;
 import com.onyx.kreader.utils.PagePositionUtils;
+
+import java.util.List;
 
 /**
  * Created by zhuzeng on 10/4/15.
@@ -184,6 +189,26 @@ public abstract class BaseReaderRequest extends BaseRequest {
         getReaderUserDataInfo().setDocumentMetadata(reader.getDocumentMetadata());
         if (readerViewInfo != null && loadPageAnnotation) {
             getReaderUserDataInfo().loadPageAnnotations(getContext(), reader, readerViewInfo.getVisiblePages());
+            if (!reader.getRendererFeatures().supportScale()) {
+                // if document doesn't support scale, means it's a flow document,
+                // then we need update selection rectangles as they may change
+                for (PageInfo pageInfo : readerViewInfo.getVisiblePages()) {
+                    List<PageAnnotation> annotations = getReaderUserDataInfo().getPageAnnotations(pageInfo);
+                    if (annotations == null) {
+                        continue;
+                    }
+                    for (PageAnnotation annotation : annotations) {
+                        ReaderHitTestManager hitTestManager = reader.getReaderHelper().getHitTestManager();
+                        ReaderSelection selection = hitTestManager.select(pageInfo.getPosition(),
+                                annotation.getAnnotation().getLocationBegin(),
+                                annotation.getAnnotation().getLocationEnd());
+                        annotation.getRectangles().clear();
+                        if (selection != null) {
+                            annotation.getRectangles().addAll(selection.getRectangles());
+                        }
+                    }
+                }
+            }
         }
         if (readerViewInfo != null && loadBookmark) {
             getReaderUserDataInfo().loadBookmarks(getContext(), reader, readerViewInfo.getVisiblePages());
