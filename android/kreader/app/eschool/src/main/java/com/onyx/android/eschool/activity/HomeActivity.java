2cbdec6394849c1fc89cc9acd756d3a511b7dcf1
wang.suicheng <wsc@onyx-international.com>
2016-12-06 10:23:10 +0800

    add pic and video display layout for home, also add settting item entrance

--- android/kreader/app/eschool/src/main/java/com/onyx/android/eschool/activity/HomeActivity.java
+++ android/kreader/app/eschool/src/main/java/com/onyx/android/eschool/activity/HomeActivity.java
@@ -1,22 +1,24 @@
 package com.onyx.android.eschool.activity;
 
 import android.content.Intent;
-import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.KeyEvent;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.onyx.android.eschool.R;
+import com.onyx.android.eschool.model.AppConfig;
 import com.onyx.android.eschool.model.StudentAccount;
 import com.onyx.android.eschool.utils.AvatarUtils;
+import com.onyx.android.eschool.utils.ResourceUtils;
+import com.onyx.android.eschool.utils.ViewDocumentUtils;
 import com.onyx.android.sdk.utils.ActivityUtil;
+import com.onyx.android.sdk.utils.StringUtils;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -29,7 +31,8 @@ import butterknife.OnClick;
 public class HomeActivity extends BaseActivity {
     private static final String EDU_INTENT_CONTENT_TYPE = "contentType";
     private static final String EDU_INTENT_CONTENT_TYPE_EXAM = "examination";
-    private static final String EDU_INTENT_CONTENT_TYPE_PRACTISE= "practice";
+    private static final String EDU_INTENT_CONTENT_TYPE_PRACTISE = "practice";
+
     private SimpleDateFormat dateFormat;
 
     // about userInfo
@@ -41,15 +44,10 @@ public class HomeActivity extends BaseActivity {
     ImageView userImage;
 
     // about notify panel
-    @Bind(R.id.text_date)
     TextView date;
-    @Bind(R.id.textView_location)
     TextView location;
-    @Bind(R.id.textView_weather_text)
     TextView weatherText;
-    @Bind(R.id.textView_notify)
     TextView notifyText;
-    @Bind(R.id.imageView_weather_image)
     ImageView weatherImage;
 
     // about teaching
@@ -57,6 +55,9 @@ public class HomeActivity extends BaseActivity {
     TextView teachingMaterialText;
     @Bind(R.id.textView_auxiliary_title)
     TextView teachingAuxiliaryText;
+
+    private String picDisplayPath = "/mnt/sdcard/slide/0037.jpg";
+    private String videoDisplayPath = "/mnt/sdcard/slide/video_display.flv";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -73,12 +74,27 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     protected Integer getLayoutId() {
-        return R.layout.activity_home;
+        return getLayoutIdByConfig();
+    }
+
+    private int getLayoutIdByConfig() {
+        AppConfig config = AppConfig.sharedInstance(this);
+        int layoutId = ResourceUtils.getLayoutResIdByName(this, config.getHomeLayout());
+        return layoutId <= 0 ? R.layout.activity_home : layoutId;
     }
 
     @Override
     protected void initView() {
         initNormalItem();
+        initViewByConfig();
+    }
+
+    protected void initViewByConfig() {
+        if (AppConfig.sharedInstance(this).isForDisplayHomeLayout()) {
+            initDisplayItemView();
+        } else {
+            initWeatherNotifyPanel();
+        }
     }
 
     @Override
@@ -88,9 +104,26 @@ public class HomeActivity extends BaseActivity {
 
     protected void initConfig() {
         dateFormat = new SimpleDateFormat(getString(R.string.home_time_date_format));
+        initConfigByAppConfig();
+    }
+
+    private void initConfigByAppConfig() {
+        if (AppConfig.sharedInstance(this).isForDisplayHomeLayout()) {
+            String path = AppConfig.sharedInstance(this).getHomePicDisplayFilePath();
+            if (StringUtils.isNotBlank(path)) {
+                picDisplayPath = path;
+            }
+            path = AppConfig.sharedInstance(this).getHomeVideoDisplayFilePath();
+            if (StringUtils.isNotBlank(path)) {
+                videoDisplayPath = path;
+            }
+        }
     }
 
     private void updateNotifyPanel() {
+        if (AppConfig.sharedInstance(this).isForDisplayHomeLayout()) {
+            return;
+        }
         date.setText(dateFormat.format(new Date()));
         loadWeatherInfo();
         loadNotifyMessage();
@@ -103,19 +136,43 @@ public class HomeActivity extends BaseActivity {
         userClass.setText(account.gradeClass);
     }
 
-    private void initNormalItem() {
-        initNormalView(R.id.home_note_item, R.string.home_item_note_text, R.drawable.home_note);
-        initNormalView(R.id.home_dictionary_item, R.string.home_item_dictionary_text, R.drawable.home_dictionary);
-        initNormalView(R.id.home_practice_item, R.string.home_item_practice_text, R.drawable.home_practice);
-        initNormalView(R.id.home_exam_item, R.string.home_item_exam_text, R.drawable.home_exam);
+    private void initWeatherNotifyPanel() {
+        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.home_weather_notify);
+        weatherImage = (ImageView) viewGroup.findViewById(R.id.imageView_weather_image);
+        weatherText = (TextView) viewGroup.findViewById(R.id.textView_weather_text);
+        date = (TextView) viewGroup.findViewById(R.id.text_date);
+        location = (TextView) viewGroup.findViewById(R.id.textView_location);
+        notifyText = (TextView) viewGroup.findViewById(R.id.textView_notify);
     }
 
-    private void initNormalView(int layoutResId, int textResId, int imageResId) {
+    private void initNormalItem() {
+        initNormalView(R.id.home_note_item, R.string.home_item_note_text, R.drawable.home_note, getNoteIntent());
+        initNormalView(R.id.home_dictionary_item, R.string.home_item_dictionary_text, R.drawable.home_dictionary, getDictIntent());
+        initNormalView(R.id.home_practice_item, R.string.home_item_practice_text, R.drawable.home_practice, getEduIntent());
+        initNormalView(R.id.home_setting_item, R.string.home_item_setting_text, R.drawable.home_setting, getSettingIntent());
+    }
+
+    private void initDisplayItemView() {
+        initNormalView(R.id.home_pic_display_item, R.string.home_item_pic_display_text, R.drawable.home_pic_display, getPicDisplayIntent());
+        initNormalView(R.id.home_video_display_item, R.string.home_item_video_display_text, R.drawable.home_video_display, getVideoPlayIntent());
+    }
+
+    private void initNormalView(int layoutResId, int textResId, int imageResId, final Intent intent) {
         ViewGroup viewGroup = (ViewGroup) findViewById(layoutResId);
         TextView textView = (TextView) viewGroup.findViewById(R.id.textView_category_text);
         textView.setText(textResId);
         ImageView imageView = (ImageView) viewGroup.findViewById(R.id.imageView_category_image);
         imageView.setImageResource(imageResId);
+        viewGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processNormalViewClick(intent);
+            }
+        });
+    }
+
+    private void processNormalViewClick(Intent intent) {
+        ActivityUtil.startActivitySafely(this, intent);
     }
 
     private void loadWeatherInfo() {
@@ -142,14 +199,20 @@ public class HomeActivity extends BaseActivity {
         ActivityUtil.startActivitySafely(this, new Intent(this, TeachingAuxiliaryActivity.class));
     }
 
-    @OnClick(R.id.home_note_item)
-    void onNoteItemClick() {
-        ActivityUtil.startActivitySafely(this, getPackageManager().getLaunchIntentForPackage("com.onyx.android.note"));
+    private Intent getPicDisplayIntent() {
+        return ViewDocumentUtils.viewActionIntentWithMimeType(new File(picDisplayPath));
     }
 
-    @OnClick(R.id.home_dictionary_item)
-    void onDictionaryItemClick() {
-        ActivityUtil.startActivitySafely(this, getPackageManager().getLaunchIntentForPackage("com.onyx.dict"));
+    private Intent getVideoPlayIntent() {
+        return ViewDocumentUtils.viewActionIntentWithMimeType(new File(videoDisplayPath));
+    }
+
+    private Intent getNoteIntent() {
+        return getPackageManager().getLaunchIntentForPackage("com.onyx.android.note");
+    }
+
+    private Intent getDictIntent() {
+        return getPackageManager().getLaunchIntentForPackage("com.onyx.dict");
     }
 
     private Intent getEduIntent() {
@@ -162,20 +225,6 @@ public class HomeActivity extends BaseActivity {
         Intent intent = new Intent();
         intent.setClassName("com.onyx.android.libsetting", "con.onyx.android.libsetting.view.activity.DeviceMainSettingActivity");
         return intent;
-    }
-
-    @OnClick(R.id.home_exam_item)
-    void onExamItemClick() {
-        Intent intent = getEduIntent();
-        intent.putExtra(EDU_INTENT_CONTENT_TYPE, EDU_INTENT_CONTENT_TYPE_EXAM);
-        ActivityUtil.startActivitySafely(this, intent);
-    }
-
-    @OnClick(R.id.home_practice_item)
-    void onPracticeItemClick() {
-        Intent intent = getEduIntent();
-        intent.putExtra(EDU_INTENT_CONTENT_TYPE, EDU_INTENT_CONTENT_TYPE_PRACTISE);
-        ActivityUtil.startActivitySafely(this, intent);
     }
 
     @Override
