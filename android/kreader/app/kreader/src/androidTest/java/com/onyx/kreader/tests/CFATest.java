2b1073c566b70055c586794efb1b24d4be14296d
john <zeng.zhu@gmail.com>
2016-12-15 22:40:59 +0800

    add image render test.

--- android/kreader/app/kreader/src/androidTest/java/com/onyx/kreader/tests/CFATest.java
+++ android/kreader/app/kreader/src/androidTest/java/com/onyx/kreader/tests/CFATest.java
@@ -1,6 +1,7 @@
 package com.onyx.kreader.tests;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.PowerManager;
 import android.test.ActivityInstrumentationTestCase2;
@@ -32,7 +33,7 @@ public class CFATest extends ActivityInstrumentationTestCase2<ReaderTestActivity
         super("com.onyx.reader", ReaderTestActivity.class);
     }
 
-    public void testRender() throws Exception {
+    public void noTestRender() throws Exception {
         NeoPdfJniWrapper wrapper = new NeoPdfJniWrapper();
         assertTrue(wrapper.nativeInitLibrary());
         assertTrue(wrapper.openDocument("/mnt/sdcard/Books/cfa.pdf", null) == NeoPdfJniWrapper.NO_ERROR);
@@ -78,12 +79,102 @@ public class CFATest extends ActivityInstrumentationTestCase2<ReaderTestActivity
         FileUtils.saveContentToFile(gray, new File("/mnt/sdcard/Books/final.bin"));
 
         // recreate image from blended data.
-        final Bitmap finalBitmap = Bitmap.createBitmap(argb.getWidth(), argb.getHeight(), Bitmap.Config.ARGB_8888);
-        ImageUtils.fromRgbw(finalBitmap, gray, strideInBytes);
+        final Bitmap finalBitmap = Bitmap.createBitmap(960, 1280, Bitmap.Config.ARGB_8888);
+        for(int i = 0; i < finalBitmap.getWidth() / 3; ++i) {
+            int step = 20;
+            int count = 0;
+            for(int j = count * step; j < count * step + step; ++j) {
+                int x = i * 2;
+                int y = j * 2;
+                finalBitmap.setPixel(x,     y,      Color.WHITE);   // b
+                finalBitmap.setPixel(x + 1, y,      Color.BLACK);   // g
+                finalBitmap.setPixel(x,     y + 1,  0xff0f0f0f);   // w
+                finalBitmap.setPixel(x + 1, y + 1,  Color.BLACK);   // r
+            }
+
+            ++count;
+            for(int j = count * step; j < count * step + step; ++j) {
+                int x = i * 2;
+                int y = j * 2;
+                finalBitmap.setPixel(x,     y,      Color.BLACK);   // r
+                finalBitmap.setPixel(x + 1, y,      Color.WHITE);   // g
+                finalBitmap.setPixel(x,     y + 1,  0xff808080);   // w
+                finalBitmap.setPixel(x + 1, y + 1,  Color.BLACK);   // b
+            }
+
+            ++count;
+            for(int j = count * step; j < count * step + step; ++j) {
+                int x = i * 2;
+                int y = j * 2;
+                finalBitmap.setPixel(x,     y,      Color.BLACK);   // b
+                finalBitmap.setPixel(x + 1, y,      Color.BLACK);   // g
+                finalBitmap.setPixel(x,     y + 1,  0xff505050);   // w
+                finalBitmap.setPixel(x + 1, y + 1,  Color.WHITE);   // r
+            }
+
+            ++count;
+            for(int j = count * step; j < count * step + step; ++j) {
+                int x = i * 2;
+                int y = j * 2;
+                finalBitmap.setPixel(x,     y,      Color.BLACK);   // b
+                finalBitmap.setPixel(x + 1, y,      Color.BLACK);   // g
+                finalBitmap.setPixel(x,     y + 1,  Color.BLACK);   // w
+                finalBitmap.setPixel(x + 1, y + 1,  Color.WHITE);   // r
+            }
+
+            ++count;
+            for(int j = count * step; j < count * step + step; ++j) {
+                int x = i * 2;
+                int y = j * 2;
+                finalBitmap.setPixel(x,     y,      Color.BLACK);   // red
+                finalBitmap.setPixel(x + 1, y,      Color.WHITE);   // green
+                finalBitmap.setPixel(x,     y + 1,  0xff808080);   // white
+                finalBitmap.setPixel(x + 1, y + 1,  Color.BLACK);   // blue
+            }
+
+            ++count;
+            for(int j = count * step; j < count * step + step; ++j) {
+                int x = i * 2;
+                int y = j * 2;
+                finalBitmap.setPixel(x,     y,      0xff808080);   // red
+                finalBitmap.setPixel(x + 1, y,      Color.BLACK);   // white
+                finalBitmap.setPixel(x,     y + 1,  Color.WHITE);   // green
+                finalBitmap.setPixel(x + 1, y + 1,  Color.BLACK);   // blue
+            }
+
+        }
         BitmapUtils.saveBitmap(finalBitmap, "/mnt/sdcard/Books/final.png");
 
         assertTrue(wrapper.closeDocument());
         assertTrue(wrapper.nativeDestroyLibrary());
+
+    }
+
+
+    public void testRender2() throws Exception {
+        //final Bitmap origin = BitmapUtils.loadBitmapFromFile("/mnt/sdcard/slide/ab2.png");
+        final Bitmap origin = BitmapUtils.loadBitmapFromFile("/mnt/sdcard/slide/ScreenShot012_42.jpg");
+        final Bitmap scaled = origin.createScaledBitmap(origin, 480, 640, true);
+        BitmapUtils.saveBitmap(scaled, "/mnt/sdcard/Books/ab-scaled.png");
+
+        final Bitmap result = Bitmap.createBitmap(960, 1280, Bitmap.Config.ARGB_8888);
+        for(int y = 0; y < scaled.getHeight(); ++y) {
+            for(int x = 0; x < scaled.getWidth(); ++x) {
+                int color = scaled.getPixel(x, y);
+                int nx = 2 * x;
+                int ny = 2 * y;
+                int b = Color.blue(color);
+                int g = Color.green(color);
+                int r = Color.red(color);
+                int w = (((r * 299) + (g * 587) + (b * 114)) / 1000) & 0xff;
+                result.setPixel(nx, ny, Color.argb(0xff, b, b, b));
+                result.setPixel(nx + 1, ny, Color.argb(0xff, g, g, g));
+                result.setPixel(nx, ny + 1, Color.argb(0xff, w, w, w));
+                result.setPixel(nx + 1, ny + 1, Color.argb(0xff, r, r, r));
+            }
+        }
+
+        BitmapUtils.saveBitmap(result, "/mnt/sdcard/Books/final8.png");
 
     }
 
