203bbe86de6d66b5a0ec05ae142f76fb98fb73bc
梁小明 <351062367@qq.com>
2016-12-16 19:40:01 +0800

    fix line seperator

--- android/kreader/app/note/src/main/java/com/onyx/android/note/actions/scribble/RemoveByGroupIdAction.java
+++ android/kreader/app/note/src/main/java/com/onyx/android/note/actions/scribble/RemoveByGroupIdAction.java
@@ -1,1 +1,41 @@
-package com.onyx.android.note.actions.scribble;import com.onyx.android.note.actions.BaseNoteAction;import com.onyx.android.note.activity.BaseScribbleActivity;import com.onyx.android.sdk.common.request.BaseCallback;import com.onyx.android.sdk.common.request.BaseRequest;import com.onyx.android.sdk.scribble.data.TouchPointList;import com.onyx.android.sdk.scribble.request.shape.ShapeRemoveByGroupIdRequest;import com.onyx.android.sdk.scribble.request.shape.ShapeRemoveByPointListRequest;import com.onyx.android.sdk.utils.StringUtils;/** * Created by ming on 12/16/16. */public class RemoveByGroupIdAction<T extends BaseScribbleActivity> extends BaseNoteAction<T> {    private ShapeRemoveByGroupIdRequest changeRequest;    private String groupId;    public RemoveByGroupIdAction(String groupId) {        this.groupId = groupId;    }    public void execute(final T activity) {        execute(activity, new BaseCallback() {            @Override            public void done(BaseRequest request, Throwable e) {                activity.onRequestFinished(changeRequest, true);            }        });    }    @Override    public void execute(final T activity, final BaseCallback callback) {        if (StringUtils.isNullOrEmpty(groupId)) {            return;        }        changeRequest = new ShapeRemoveByGroupIdRequest(groupId);        activity.submitRequest(changeRequest, callback);    }}
+package com.onyx.android.note.actions.scribble;
+
+import com.onyx.android.note.actions.BaseNoteAction;
+import com.onyx.android.note.activity.BaseScribbleActivity;
+import com.onyx.android.sdk.common.request.BaseCallback;
+import com.onyx.android.sdk.common.request.BaseRequest;
+import com.onyx.android.sdk.scribble.data.TouchPointList;
+import com.onyx.android.sdk.scribble.request.shape.ShapeRemoveByGroupIdRequest;
+import com.onyx.android.sdk.scribble.request.shape.ShapeRemoveByPointListRequest;
+import com.onyx.android.sdk.utils.StringUtils;
+
+/**
+ * Created by ming on 12/16/16.
+ */
+public class RemoveByGroupIdAction<T extends BaseScribbleActivity> extends BaseNoteAction<T> {
+    private ShapeRemoveByGroupIdRequest changeRequest;
+    private String groupId;
+
+    public RemoveByGroupIdAction(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public void execute(final T activity) {
+        execute(activity, new BaseCallback() {
+            @Override
+            public void done(BaseRequest request, Throwable e) {
+                activity.onRequestFinished(changeRequest, true);
+            }
+        });
+    }
+
+    @Override
+    public void execute(final T activity, final BaseCallback callback) {
+        if (StringUtils.isNullOrEmpty(groupId)) {
+            return;
+        }
+        changeRequest = new ShapeRemoveByGroupIdRequest(groupId);
+        activity.submitRequest(changeRequest, callback);
+    }
+
+}
