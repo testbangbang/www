d3bf8b0b9e5ebf3443f7243c86f2ddab6469fbfb
john <zeng.zhu@gmail.com>
2016-12-16 20:48:06 +0800

    add test to dump all image files.

--- android/kreader/app/kreader/src/androidTest/java/com/onyx/kreader/tests/CFATest.java
+++ android/kreader/app/kreader/src/androidTest/java/com/onyx/kreader/tests/CFATest.java
@@ -3,8 +3,11 @@ package com.onyx.kreader.tests;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 
+import com.onyx.android.sdk.data.ReaderBitmapImpl;
 import com.onyx.android.sdk.utils.BitmapUtils;
+import com.onyx.kreader.plugins.neopdf.NeoPdfJniWrapper;
 import com.onyx.kreader.utils.ImageUtils;
 
 /**
@@ -18,6 +21,45 @@ public class CFATest extends ActivityInstrumentationTestCase2<ReaderTestActivity
 
     public CFATest() {
         super("com.onyx.reader", ReaderTestActivity.class);
+    }
+
+    public void testRender1() throws Exception {
+        NeoPdfJniWrapper wrapper = new NeoPdfJniWrapper();
+        String[] pathes = {"/mnt/sdcard/Books/cfa.pdf"};
+        for (String path : pathes) {
+            assertTrue(wrapper.nativeInitLibrary());
+            assertTrue(wrapper.openDocument(path, null) == NeoPdfJniWrapper.NO_ERROR);
+            int pageCount = wrapper.pageCount();
+            assertTrue(pageCount > 0);
+            ReaderBitmapImpl readerBitmap = null;
+            for (int i = 0; i < pageCount; ++i) {
+                float size[] = new float[2];
+                assertTrue(wrapper.pageSize(i, size));
+                assertTrue(size[0] > 0);
+                assertTrue(size[1] > 0);
+                if (readerBitmap == null) {
+                    readerBitmap = new ReaderBitmapImpl(480, 640, Bitmap.Config.ARGB_8888);
+                } else {
+                    readerBitmap.update(480, 640, Bitmap.Config.ARGB_8888);
+                }
+                Bitmap bitmap = readerBitmap.getBitmap();
+                long start = System.currentTimeMillis();
+                assertTrue(wrapper.drawPage(i, 0, 0, bitmap.getWidth(), bitmap.getHeight(), 0, bitmap));
+                long end = System.currentTimeMillis();
+                Log.i(TAG, "Performance testing: " + path + " page: " + i + " ts: " + (end - start));
+
+                BitmapUtils.saveBitmap(bitmap, "/mnt/sdcard/Books/cfa.origin." + i + ".png");
+
+                final Bitmap result = Bitmap.createBitmap(960, 1280, Bitmap.Config.ARGB_8888);
+                ImageUtils.toRgbwBitmap(result, bitmap, 0);
+                BitmapUtils.saveBitmap(result, "/mnt/sdcard/Books/cfa." + i + ".png");
+
+
+            }
+            readerBitmap.recycleBitmap();
+            assertTrue(wrapper.closeDocument());
+            assertTrue(wrapper.nativeDestroyLibrary());
+        }
     }
 
     public void testRender2() throws Exception {
