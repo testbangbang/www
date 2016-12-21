6423686efb5c4f8841dcacfb3fe5ba9da005b5ca
wang.suicheng <wsc@onyx-international.com>
2016-12-21 15:33:05 +0800

    remove video display item and adjust icon and text of eschool home

--- android/kreader/app/eschool/src/main/java/com/onyx/android/eschool/activity/HomeActivity.java
+++ android/kreader/app/eschool/src/main/java/com/onyx/android/eschool/activity/HomeActivity.java
@@ -155,7 +155,7 @@ public class HomeActivity extends BaseActivity {
 
     private void initDisplayItemView() {
         initNormalView(R.id.home_pic_display_item, R.string.home_item_pic_display_text, R.drawable.home_pic_display, getPicDisplayIntent());
-        initNormalView(R.id.home_video_display_item, R.string.home_item_video_display_text, R.drawable.home_video_display, getVideoPlayIntent());
+        //initNormalView(R.id.home_video_display_item, R.string.home_item_video_display_text, R.drawable.home_video_display, getVideoPlayIntent());
     }
 
     private void initNormalView(int layoutResId, int textResId, int imageResId, final Intent intent) {
