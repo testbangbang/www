969948fc9a388c03c8a1f7da3e24aa67de01cd97
Joy <joycodex@hotmail.com>
2016-12-27 17:53:45 +0800

    kreader: fix error of code page not applied in AlReaderWrapper

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/plugins/alreader/AlReaderWrapper.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/plugins/alreader/AlReaderWrapper.java
@@ -92,7 +92,7 @@ public class AlReaderWrapper {
     public long openDocument(final String path,  final ReaderDocumentOptions documentOptions) {
         filePath = path;
         AlBookOptions bookOpt = new AlBookOptions();
-        bookOpt.codePage = TAL_CODE_PAGES.AUTO;
+        bookOpt.codePage = documentOptions.getCodePage();
         bookOpt.codePageDefault = documentOptions.getCodePage();
         bookOpt.formatOptions = 0;
         bookOpt.readPosition = 0;
