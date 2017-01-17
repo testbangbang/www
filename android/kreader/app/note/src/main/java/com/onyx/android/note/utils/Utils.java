9597af1a911e4a7809da3dd1d508651917b7ef22
Solskjaer49 <solskjaer49@gmail.com>
2017-01-17 16:16:47 +0800

    Fix Incorrect Note Title Name Problem.

--- android/kreader/app/note/src/main/java/com/onyx/android/note/utils/Utils.java
+++ android/kreader/app/note/src/main/java/com/onyx/android/note/utils/Utils.java
@@ -10,7 +10,6 @@ import com.onyx.android.sdk.data.GAdapter;
 import com.onyx.android.sdk.data.GAdapterUtil;
 import com.onyx.android.sdk.data.GObject;
 import com.onyx.android.sdk.scribble.data.NoteModel;
-import com.onyx.android.sdk.scribble.data.PageNameList;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -78,7 +77,7 @@ public class Utils {
     }
 
     public static GObject createLibraryItem(final NoteModel noteModel, final int folderRes) {
-        GObject object = GAdapterUtil.createTableItem(getNoteTitleWithSize(noteModel), null, 0, 0, null);
+        GObject object = GAdapterUtil.createTableItem(noteModel.getTitle(), null, 0, 0, null);
         object.putString(GAdapterUtil.TAG_UNIQUE_ID, noteModel.getUniqueId());
         object.putNonNullObject(GAdapterUtil.TAG_THUMBNAIL, folderRes);
         object.putString(GAdapterUtil.TAG_SUB_TITLE_STRING, getDateFormat().format(noteModel.getUpdatedAt()));
@@ -87,23 +86,12 @@ public class Utils {
     }
 
     public static GObject createDocumentItem(final NoteModel noteModel, final int docRes) {
-        GObject object = GAdapterUtil.createTableItem(getNoteTitleWithSize(noteModel), null, 0, 0, null);
+        GObject object = GAdapterUtil.createTableItem(noteModel.getTitle(), null, 0, 0, null);
         object.putString(GAdapterUtil.TAG_UNIQUE_ID, noteModel.getUniqueId());
         object.putString(GAdapterUtil.TAG_SUB_TITLE_STRING, getDateFormat().format(noteModel.getUpdatedAt()));
         object.putNonNullObject(GAdapterUtil.TAG_THUMBNAIL, noteModel.getThumbnail() == null ? docRes : noteModel.getThumbnail());
         putItemType(object, TYPE_DOCUMENT);
         return object;
-    }
-
-    private static String getNoteTitleWithSize(final NoteModel noteModel) {
-        StringBuilder builder = new StringBuilder(noteModel.getTitle());
-        PageNameList pageNameList = noteModel.getPageNameList();
-        if (pageNameList != null && pageNameList.size() > 0) {
-            builder.append("(");
-            builder.append(pageNameList.size());
-            builder.append(")");
-        }
-        return builder.toString();
     }
 
     public static GObject createNoteItem(final NoteModel noteModel, final int folderRes, final int docRes) {
