4bfa7edf12d4541d550802abdb957194188abc0e
梁小明 <351062367@qq.com>
2017-02-17 16:07:07 +0800

    fix booktype value wrong

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/statistics/ReadRecordFragment.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/statistics/ReadRecordFragment.java
@@ -225,9 +225,9 @@ public class ReadRecordFragment extends StatisticsFragment {
 
                 setDashLineVisibility(viewHolder, position, count, cols);
                 String title = "";
-                int value = 0;
+                double value = 0;
                 if (statisticsResult != null && statisticsResult.getBookTypeAgg() != null ) {
-                    LinkedMap<String, Integer> bookTypeAgg = statisticsResult.getBookTypeAgg();
+                    LinkedMap<String, Double> bookTypeAgg = statisticsResult.getBookTypeAgg();
                     if (position < bookTypeAgg.size()) {
                         String typeId = bookTypeAgg.get(position);
                         if (categoryMap != null) {
@@ -235,7 +235,7 @@ public class ReadRecordFragment extends StatisticsFragment {
                             title = BookCategoryUtils.getCategoryName(getContext(), category);
                             hasCategorys.add(category);
                         }
-                        value = bookTypeAgg.getValue(position);
+                        value = bookTypeAgg.getValue(position) * 100;
                     }else {
                         for (BookCategory category : BookCategory.values()) {
                             if (!hasCategorys.contains(category)) {
@@ -249,7 +249,7 @@ public class ReadRecordFragment extends StatisticsFragment {
                     title = getString(categoryTitleIDs[position]);
                 }
                 viewHolder.setText(R.id.text_category, title);
-                viewHolder.setText(R.id.text_proportion, String.format("%d%%", value));
+                viewHolder.setText(R.id.text_proportion, String.format("%d%%", (int) value));
             }
         });
     }
