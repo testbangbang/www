73f4e7942b39ff81231a9197eb6bc572765b6778
wang.suicheng <wsc@onyx-international.com>
2017-02-18 16:42:33 +0800

    add onyxsdk-ebookservice module

---
+++ android/kreader/libraries/onyxsdk-ebookservice/src/main/java/com/onyx/sdk/ebookservice/Constant.java
@@ -0,0 +1,21 @@
+package com.onyx.sdk.ebookservice;
+
+/**
+ * Created by suicheng on 2017/2/12.
+ */
+public class Constant {
+    public static final String IMG_HREF = "images/";
+    public static final String TITLE_TAG = "title";
+    public static final String IMG_TAG = "img";
+    public static final String HTTP_PREFIX = "http://";
+    public static final String HTTPS_PREFIX = "https://";
+
+    public static final String SRC_ATTRIBUTE = "src";
+
+    public static final String XHTML_EXTENSION = ".xhtml";
+    public static final String EPUB_EXTENSION = ".epub";
+    public static final String PNG_EXTENSION = ".png";
+
+    public static final String CHAPTER_PATTERN = "chapter%d";
+    public static final String COVER_NAME = "cover" + PNG_EXTENSION;
+}
