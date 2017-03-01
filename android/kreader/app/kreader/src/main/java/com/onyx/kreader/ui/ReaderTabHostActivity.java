8d12978b668b12d30d1d138e17efd2e3a3b42b59
Joy <joy@onyx-international.com>
2017-03-01 12:10:59 +0800

    kreader: handle document's orientation in ReaderTabHostActivity

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/ReaderTabHostActivity.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/ReaderTabHostActivity.java
@@ -3,6 +3,7 @@ package com.onyx.kreader.ui;
 import android.app.ActivityManager;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.PowerManager;
@@ -17,7 +18,12 @@ import android.widget.TabWidget;
 import android.widget.TextView;
 
 import com.alibaba.fastjson.JSON;
+import com.onyx.android.sdk.common.request.BaseCallback;
+import com.onyx.android.sdk.common.request.BaseRequest;
+import com.onyx.android.sdk.data.DataManager;
 import com.onyx.android.sdk.device.Device;
+import com.onyx.android.sdk.reader.common.Debug;
+import com.onyx.android.sdk.reader.host.options.BaseOptions;
 import com.onyx.android.sdk.reader.utils.TreeObserverUtils;
 import com.onyx.android.sdk.utils.DeviceUtils;
 import com.onyx.android.sdk.utils.FileUtils;
@@ -25,6 +31,7 @@ import com.onyx.android.sdk.utils.StringUtils;
 import com.onyx.kreader.R;
 import com.onyx.kreader.device.DeviceConfig;
 import com.onyx.kreader.ui.data.SingletonSharedPreference;
+import com.onyx.kreader.ui.requests.LoadDocumentOptionsRequest;
 
 import java.io.File;
 import java.util.HashMap;
@@ -69,6 +76,7 @@ public class ReaderTabHostActivity extends AppCompatActivity {
     private PowerManager.WakeLock startupWakeLock;
 
     private TabHost tabHost;
+    private String pathToContinueOpenAfterRotation;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -110,15 +118,58 @@ public class ReaderTabHostActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
-        Log.d(TAG, "onResume");
+        Log.d(TAG, "onResume, tab count: " + tabHost.getTabWidget().getTabCount());
         super.onResume();
 
         syncFullScreenState();
 
-        int orientation = SingletonSharedPreference.getScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        if (orientation != DeviceUtils.getScreenOrientation(this)) {
-            setRequestedOrientation(orientation);
-        }
+//        int orientation = SingletonSharedPreference.getScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//        if (orientation != DeviceUtils.getScreenOrientation(this)) {
+//            setRequestedOrientation(orientation);
+//        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        Log.d(TAG, "onConfigurationChanged, tab count: " + tabHost.getTabWidget().getTabCount());
+        tabHost.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                Log.d(TAG, "onChangeOrientation -> onGlobalLayout");
+                TreeObserverUtils.removeGlobalOnLayoutListener(tabHost.getViewTreeObserver(), this);
+                updateWindowHeight();
+                if (StringUtils.isNotBlank(pathToContinueOpenAfterRotation)) {
+                    openDocWithTab(pathToContinueOpenAfterRotation);
+                    pathToContinueOpenAfterRotation = null;
+                }
+            }
+        });
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart");
+        super.onRestart();
     }
 
     @Override
@@ -126,6 +177,12 @@ public class ReaderTabHostActivity extends AppCompatActivity {
         Log.d(TAG, "onDestroy");
         super.onDestroy();
         releaseStartupWakeLock();
+    }
+
+    @Override
+    public void onLowMemory() {
+        Log.e(TAG, "onLowMemory");
+        super.onLowMemory();
     }
 
     @Override
@@ -157,17 +214,17 @@ public class ReaderTabHostActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onLowMemory() {
-        Log.e(TAG, "onLowMemory");
-        super.onLowMemory();
+    private boolean isMultipleTabbed() {
+       return tabActivityList.size() > 0;
     }
 
     private void initTabHost() {
         tabHost = (TabHost) findViewById(R.id.tab_host);
         tabHost.setup();
 
-//        addTabToHost(ReaderTab.TAB_1, "TAB");
+        if (isMultipleTabbed()) {
+            addTabToHost(ReaderTab.TAB_1, "TAB");
+        }
 
         tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
             @Override
@@ -295,7 +352,45 @@ public class ReaderTabHostActivity extends AppCompatActivity {
 
     private void handleViewActionIntent() {
         final String path = FileUtils.getRealFilePathFromUri(this, getIntent().getData());
+        final LoadDocumentOptionsRequest loadDocumentOptionsRequest = new LoadDocumentOptionsRequest(path,
+                null);
+        DataManager dataProvider = new DataManager();
+        dataProvider.submit(this, loadDocumentOptionsRequest, new BaseCallback() {
+            @Override
+            public void done(BaseRequest request, Throwable e) {
+                if (e != null) {
+                    return;
+                }
+                if (!processOrientation(loadDocumentOptionsRequest.getDocumentOptions())) {
+                    pathToContinueOpenAfterRotation = path;
+                    return;
+                }
+                openDocWithTab(path);
+            }
+        });
 
+    }
+
+    private boolean processOrientation(final BaseOptions options) {
+        int target = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+        if (options != null && options.getOrientation() >= 0) {
+            target = options.getOrientation();
+        }
+        int current = DeviceUtils.getScreenOrientation(this);
+        Debug.d("current orientation: " + current + ", target orientation: " + target);
+        if (current != target) {
+            setRequestedOrientation(target);
+            if (target == ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT) {
+                // reverse portrait will not trigger onConfigurationChanged() in activity,
+                // so we process as orientation not changed
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private void openDocWithTab(String path) {
         ReaderTab tab = findOpenedTabByPath(path);
         if (tab != null) {
             Log.d(TAG, "file already opened in tab: " + tab + ", " + getIntent().getDataString());
@@ -308,7 +403,7 @@ public class ReaderTabHostActivity extends AppCompatActivity {
         tab = getFreeTab();
         Intent intent = new Intent(this, getTabActivity(tab));
         intent.setAction(Intent.ACTION_VIEW);
-        intent.setDataAndType(getIntent().getData(), getIntent().getType());
+        intent.setDataAndType(Uri.fromFile(new File(path)), getIntent().getType());
         final int tabContentHeight = getTabContentHeight();
         intent.putExtra(ReaderBroadcastReceiver.TAG_WINDOW_HEIGHT, tabContentHeight);
         startActivity(intent);
@@ -318,13 +413,10 @@ public class ReaderTabHostActivity extends AppCompatActivity {
 
     private void addReaderTab(ReaderTab tab, String path) {
         addOpenedTab(tab, path);
-        if (openedTabs.size() < 2) {
+        if (!isMultipleTabbed()) {
             return;
-        } else if (openedTabs.size() == 2) {
-             ReaderTab firstTab = openedTabs.keySet().iterator().next();
-            addTabToHost(firstTab, openedTabs.get(firstTab));
-            updateWindowHeight();
         }
+
         addTabToHost(tab, path);
         updateCurrentTab(tab);
     }
@@ -332,12 +424,10 @@ public class ReaderTabHostActivity extends AppCompatActivity {
     private void closeReaderTab(ReaderTab tab) {
         closeTabActivity(tab);
         removeOpenedTab(tab);
+        removeTabFromHost(tab);
 
-        if (openedTabs.size() > 1) {
-            removeTabFromHost(tab);
-        } else {
-            tabHost.clearAllTabs();
-            updateWindowHeight();
+        if (openedTabs.size() <= 0) {
+            finish();
         }
     }
 
