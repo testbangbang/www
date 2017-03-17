f0ba5993f05675cf9d6ba77f9bf349e6c5f2f5eb
wangxu <wangxu@onyx-international.com>
2017-03-17 20:19:46 +0800

    clean code.

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/dialog/DialogTts.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/dialog/DialogTts.java
@@ -7,6 +7,7 @@ import android.util.Pair;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.CheckBox;
