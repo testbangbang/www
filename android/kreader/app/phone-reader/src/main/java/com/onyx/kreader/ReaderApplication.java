84889f17338537395c2d6cd45e500c626159287e
wang.suicheng <wsc@onyx-international.com>
2017-03-24 10:55:06 +0800

    phone-reader: fix shared url issue and use new OssManager

--- android/kreader/app/phone-reader/src/main/java/com/onyx/kreader/ReaderApplication.java
+++ android/kreader/app/phone-reader/src/main/java/com/onyx/kreader/ReaderApplication.java
@@ -1,8 +1,10 @@
 package com.onyx.kreader;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.onyx.android.sdk.data.CloudStore;
+import com.onyx.android.sdk.data.manager.OssManager;
 import com.onyx.android.sdk.data.manager.WeChatManager;
 
 /**
@@ -10,6 +12,11 @@ import com.onyx.android.sdk.data.manager.WeChatManager;
  */
 
 public class ReaderApplication extends Application {
+
+    static public final String OSS_PUSH_KEY_ID = "LTAIn6vAi9dxw9IS";
+    static public final String OSS_PUSH_KEY_SECRET = "zagMGgELcUpWwaxXkQPoUNcJBwBYRT";
+    static public final String OSS_PUSH_BUCKET = "onyx-content";
+    static public final String OSS_PUSH_ENDPOINT = "http://content.onyx-international.cn";
 
     static public final String WX_APP_ID = "wx86d74b4e0a1d83d0";
     static public final String WX_APP_SECRETE = "4bd220f283d7858ad4e071c0ea998d6b";
@@ -49,5 +56,14 @@ public class ReaderApplication extends Application {
 
     public static ReaderApplication instance() {
         return sInstance;
+    }
+
+    public static OssManager getPushOssManger(Context context) {
+        OssManager.OssConfig ossConfig = new OssManager.OssConfig();
+        ossConfig.setBucketName(OSS_PUSH_BUCKET);
+        ossConfig.setEndPoint(OSS_PUSH_ENDPOINT);
+        ossConfig.setKeyId(OSS_PUSH_KEY_ID);
+        ossConfig.setKeySecret(OSS_PUSH_KEY_SECRET);
+        return new OssManager(context.getApplicationContext(), ossConfig);
     }
 }
