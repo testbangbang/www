7ddcafc0842c882f0f04700211bbc95e69a2ec50
wang.suicheng <wsc@onyx-international.com>
2017-03-27 16:19:36 +0800

    onyxsdk-data: add logCollection func

---
+++ android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/model/LogCollection.java
@@ -0,0 +1,14 @@
+package com.onyx.android.sdk.data.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by suicheng on 2017/3/22.
+ */
+
+public class LogCollection implements Serializable {
+
+    public Firmware firmware;
+    public String desc;
+    public String zipFile;
+}
