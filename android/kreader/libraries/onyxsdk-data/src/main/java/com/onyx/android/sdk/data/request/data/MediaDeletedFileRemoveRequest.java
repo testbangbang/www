e0a00d6dc267718dd83991cbdbbe235c3f608a7f
wang.suicheng <wsc@onyx-international.com>
2017-03-28 17:50:55 +0800

    eschool: add MediaScan func via json config

---
+++ android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/request/data/MediaDeletedFileRemoveRequest.java
@@ -0,0 +1,50 @@
+package com.onyx.android.sdk.data.request.data;
+
+import android.content.ContentResolver;
+import android.provider.MediaStore;
+
+import com.onyx.android.sdk.data.DataManager;
+import com.onyx.android.sdk.utils.CollectionUtils;
+import com.onyx.android.sdk.utils.FileUtils;
+import com.onyx.android.sdk.utils.MimeTypeUtils;
+
+import java.util.List;
+
+/**
+ * Created by suicheng on 2017/3/28.
+ */
+
+public class MediaDeletedFileRemoveRequest extends BaseDataRequest {
+
+    private List<String> targetPathList;
+
+    public MediaDeletedFileRemoveRequest(List<String> targetPathList) {
+        this.targetPathList = targetPathList;
+    }
+
+    @Override
+    public void execute(DataManager dataManager) throws Exception {
+        proceedDeletedFileMediaInformation();
+    }
+
+    private void proceedDeletedFileMediaInformation() {
+        if (CollectionUtils.isNullOrEmpty(targetPathList)) {
+            return;
+        }
+        ContentResolver resolver = getContext().getContentResolver();
+        for (String deletedFilePath : targetPathList) {
+            try {
+                if (MimeTypeUtils.isAudioFile(FileUtils.getFileExtension(deletedFilePath))) {
+                    resolver.delete(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                            MediaStore.Audio.Media.DATA + "=" + "'" + deletedFilePath + "'", null);
+                } else {
+                    resolver.delete(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            MediaStore.Images.Media.DATA + "=" + "'" + deletedFilePath + "'", null);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        targetPathList.clear();
+    }
+}
