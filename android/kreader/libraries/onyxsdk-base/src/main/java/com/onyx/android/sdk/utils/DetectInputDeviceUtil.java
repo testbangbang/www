7f4f4483dcb68e1e0023cc7c32d1f6f050ed509a
wangxu <wangxu@onyx-international.com>
2017-04-10 11:29:24 +0800

    adjust detect input device method.

--- android/kreader/libraries/onyxsdk-base/src/main/java/com/onyx/android/sdk/utils/DetectInputDeviceUtil.java
+++ android/kreader/libraries/onyxsdk-base/src/main/java/com/onyx/android/sdk/utils/DetectInputDeviceUtil.java
@@ -2,6 +2,9 @@ package com.onyx.android.sdk.utils;
 
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by wangxu on 17-3-18.
@@ -10,9 +13,9 @@ import java.io.File;
 public class DetectInputDeviceUtil {
 
     private static final String INPUT_DEVICES_INFO_FILE = "/proc/bus/input/devices";
-    private static final String HANVON_TP_NAME = "hanvon_tp";
     private static final String SPLIT_TAG_B = "B:";
     private static final String MATCH_TAG_HANDLERS = "Handlers";
+    private static final List<String> TP_NAME = new ArrayList<String>(Arrays.asList("hanvon_tp", "Wacom"));
 
     public static String detectInputDevicePath() {
         String result = null;
@@ -20,15 +23,13 @@ public class DetectInputDeviceUtil {
             String content = FileUtils.readContentOfFile(new File(INPUT_DEVICES_INFO_FILE));
             if (StringUtils.isNotBlank(content)) {
                 String[] splitBstr = split(content, SPLIT_TAG_B);
-                if (null != splitBstr) {
-                    String matchTpStr = match(splitBstr, HANVON_TP_NAME);
-                    if (StringUtils.isNotBlank(matchTpStr)) {
-                        String[] splitStr = split(matchTpStr, " ");
-                        if (null != splitStr) {
-                            String matchStr = match(splitStr, MATCH_TAG_HANDLERS);
-                            if (StringUtils.isNotBlank(matchStr)) {
-                                result = matchStr.substring(matchStr.length() -1);
-                            }
+                String matchTpStr = matchTpStr(splitBstr);
+                if (StringUtils.isNotBlank(matchTpStr)) {
+                    String[] splitStr = split(matchTpStr, " ");
+                    if (null != splitStr) {
+                        String matchStr = match(splitStr, MATCH_TAG_HANDLERS);
+                        if (StringUtils.isNotBlank(matchStr)) {
+                            result = matchStr.substring(matchStr.length() -1);
                         }
                     }
                 }
@@ -56,5 +57,18 @@ public class DetectInputDeviceUtil {
             }
         }
         return result;
+    }
+
+    private static String matchTpStr(String[] content) {
+        String matchTpStr = null;
+        if (null != content) {
+            for (String tp : TP_NAME) {
+                matchTpStr = match(content, tp);
+                if (StringUtils.isNotBlank(matchTpStr)) {
+                    break;
+                }
+            }
+        }
+        return matchTpStr;
     }
 }
