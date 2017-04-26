76d80093cdca739788981e07b06857416cb04172
Joy <joycodex@hotmail.com>
2017-04-26 11:25:34 +0800

    kreader: handle media unmounted event in ReaderTabHostActivity

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/ReaderTabHostActivity.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/ReaderTabHostActivity.java
@@ -23,10 +23,12 @@ import com.onyx.android.sdk.common.request.BaseCallback;
 import com.onyx.android.sdk.common.request.BaseRequest;
 import com.onyx.android.sdk.common.request.WakeLockHolder;
 import com.onyx.android.sdk.data.DataManager;
+import com.onyx.android.sdk.device.EnvironmentUtil;
 import com.onyx.android.sdk.ui.dialog.OnyxCustomDialog;
 import com.onyx.android.sdk.utils.Debug;
 import com.onyx.android.sdk.reader.host.options.BaseOptions;
 import com.onyx.android.sdk.reader.utils.TreeObserverUtils;
+import com.onyx.android.sdk.utils.DeviceReceiver;
 import com.onyx.android.sdk.utils.DeviceUtils;
 import com.onyx.android.sdk.utils.FileUtils;
 import com.onyx.android.sdk.utils.StringUtils;
@@ -58,6 +60,8 @@ public class ReaderTabHostActivity extends OnyxBaseActivity {
 
     private boolean insideTabChanging = false;
     private boolean isManualShowTab = true;
+
+    private DeviceReceiver deviceReceiver = new DeviceReceiver();
 
     public static void setTabWidgetVisible(boolean visible) {
         ReaderTabHostActivity.tabWidgetVisible.set(visible);
@@ -114,6 +118,7 @@ public class ReaderTabHostActivity extends OnyxBaseActivity {
 
     @Override
     protected void onDestroy() {
+        deviceReceiver.enable(this, false);
         saveReaderTabState();
         super.onDestroy();
         releaseStartupWakeLock();
@@ -192,6 +197,21 @@ public class ReaderTabHostActivity extends OnyxBaseActivity {
         }
     }
 
+    private boolean closeReaderIfFileNotExists() {
+        final OnyxCustomDialog dlg = OnyxCustomDialog.getConfirmDialog(ReaderTabHostActivity.this,
+                getResources().getString(R.string.file_not_exists),
+                false,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        onBackPressed();
+                    }
+                }, null);
+        bringSelfToFront();
+        dlg.show();
+        return true;
+    }
+
     private boolean closeTabIfFileNotExists(final ReaderTabManager.ReaderTab tab) {
         File file = new File(tabManager.getOpenedTabs().get(tab));
         if (file.exists()) {
@@ -258,6 +278,41 @@ public class ReaderTabHostActivity extends OnyxBaseActivity {
                 enableDebugLog(false);
             }
         });
+
+        deviceReceiver.initReceiver(this);
+        deviceReceiver.setMediaStateListener(new DeviceReceiver.MediaStateListener() {
+
+            @Override
+            public void onMediaUnmounted(Intent intent) {
+                Debug.d(TAG, "onMediaUnmounted: " + intent);
+                if (EnvironmentUtil.isExternalStorageDirectory(ReaderTabHostActivity.this, intent)) {
+                    // when device is connected to PC as UMS, close reader for safe
+                    deviceReceiver.enable(ReaderTabHostActivity.this, false);
+                    onBackPressed();
+                } else {
+                    ReaderTabHostActivity.this.onMediaUnmounted(intent);
+                }
+            }
+
+            @Override
+            public void onMediaBadRemoval(Intent intent) {
+                Debug.d(TAG, "onMediaBadRemoval: " + intent);
+                ReaderTabHostActivity.this.onMediaUnmounted(intent);
+            }
+        });
+    }
+
+    private void onMediaUnmounted(Intent intent) {
+        final String media = FileUtils.getRealFilePathFromUri(this, intent.getData());
+        ReaderTabManager.ReaderTab tab = getCurrentTabInHost();
+        if (!tabManager.isTabOpened(tab)) {
+            return;
+        }
+        String path = tabManager.getOpenedTabs().get(tab);
+        if (!path.startsWith(media)) {
+            return;
+        }
+        closeReaderIfFileNotExists();
     }
 
     private boolean isReverseOrientation(int current, int target) {
@@ -741,5 +796,4 @@ public class ReaderTabHostActivity extends OnyxBaseActivity {
             }
         }
     }
-
 }
