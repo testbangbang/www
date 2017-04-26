b9ddfb87cbdae29422286c3bf02505ac52f37f30
梁小明 <351062367@qq.com>
2017-04-26 15:26:34 +0800

    clean flushReaderNote code

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/ReaderActivity.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/ReaderActivity.java
@@ -666,16 +666,12 @@ public class ReaderActivity extends OnyxBaseActivity {
 
     @Subscribe
     public void onDFBShapeFinished(final ShapeAddedEvent event) {
-        final List<PageInfo> list = getReaderDataHolder().getVisiblePages();
-        FlushNoteAction flushNoteAction = new FlushNoteAction(list, true, false, false, false);
-        flushNoteAction.execute(getReaderDataHolder(), null);
+        flushReaderNote(true, false, false, false, null);
     }
 
     private void prepareForErasing() {
         if (getReaderDataHolder().getNoteManager().hasShapeStash()) {
-            final List<PageInfo> list = getReaderDataHolder().getVisiblePages();
-            final FlushNoteAction flushNoteAction = new FlushNoteAction(list, true, true, false, false);
-            flushNoteAction.execute(getReaderDataHolder(), null);
+            flushReaderNote(true, true, false, false, null);
             return;
         }
         boolean drawDuringErasing = false;
@@ -785,9 +781,7 @@ public class ReaderActivity extends OnyxBaseActivity {
         updateNoteHostView();
 
         if (getReaderDataHolder().inNoteWritingProvider()) {
-            final List<PageInfo> list = getReaderDataHolder().getVisiblePages();
-            FlushNoteAction flushNoteAction = new FlushNoteAction(list, true, true, true, false);
-            flushNoteAction.execute(getReaderDataHolder(), new BaseCallback() {
+            flushReaderNote(true, true, true, false, new BaseCallback() {
                 @Override
                 public void done(BaseRequest request, Throwable e) {
                     changeViewConfig();
@@ -796,6 +790,17 @@ public class ReaderActivity extends OnyxBaseActivity {
         }else {
             changeViewConfig();
         }
+    }
+
+    private void flushReaderNote(boolean renderShapes, boolean transferBitmap, boolean saveToDatabase, boolean show, final BaseCallback callback) {
+        final List<PageInfo> list = getReaderDataHolder().getVisiblePages();
+        FlushNoteAction flushNoteAction = new FlushNoteAction(list, renderShapes, transferBitmap, saveToDatabase, show);
+        flushNoteAction.execute(getReaderDataHolder(), new BaseCallback() {
+            @Override
+            public void done(BaseRequest request, Throwable e) {
+                BaseCallback.invoke(callback, request, e);
+            }
+        });
     }
 
     private void changeViewConfig() {
@@ -941,9 +946,7 @@ public class ReaderActivity extends OnyxBaseActivity {
             getHandlerManager().setEnableTouch(true);
             return;
         }
-        final List<PageInfo> list = getReaderDataHolder().getVisiblePages();
-        FlushNoteAction flushNoteAction = new FlushNoteAction(list, true, true, false, false);
-        flushNoteAction.execute(getReaderDataHolder(), new BaseCallback() {
+        flushReaderNote(true, true, false, false, new BaseCallback() {
             @Override
             public void done(BaseRequest request, Throwable e) {
                 getHandlerManager().setEnableTouch(true);
