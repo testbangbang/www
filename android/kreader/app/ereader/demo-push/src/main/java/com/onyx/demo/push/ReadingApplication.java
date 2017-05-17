eba6162b92c8d3519f37ced6d215fdb2203048ff
john <zeng.zhu@gmail.com>
2017-05-17 23:13:17 +0800

    fix build break.

--- android/kreader/app/ereader/demo-push/src/main/java/com/onyx/demo/push/ReadingApplication.java
+++ android/kreader/app/ereader/demo-push/src/main/java/com/onyx/demo/push/ReadingApplication.java
@@ -1,6 +1,10 @@
 package com.onyx.demo.push;
 
 import android.app.Application;
+import android.content.Context;
+
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.onyx.android.sdk.data.OnyxDownloadManager;
 import com.onyx.demo.push.manager.LeanCloudManager;
@@ -8,9 +12,15 @@ import com.onyx.demo.push.manager.LeanCloudManager;
 /**
  * Created by suicheng on 2017/3/3.
  */
-public class ReadingApplication extends Application {
+public class ReadingApplication extends MultiDexApplication {
 
     static private ReadingApplication sInstance;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(ReadingApplication.this);
+    }
 
     @Override
     public void onCreate() {
