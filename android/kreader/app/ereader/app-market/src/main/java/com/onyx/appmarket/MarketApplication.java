6a0d1e9c7b291b24177c26fa6a4217b92a6222b1
john <zeng.zhu@gmail.com>
2017-05-18 11:08:38 +0800

    fix multidex issue.

--- android/kreader/app/ereader/app-market/src/main/java/com/onyx/appmarket/MarketApplication.java
+++ android/kreader/app/ereader/app-market/src/main/java/com/onyx/appmarket/MarketApplication.java
@@ -1,6 +1,10 @@
 package com.onyx.appmarket;
 
 import android.app.Application;
+import android.content.Context;
+
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.onyx.android.sdk.data.CloudStore;
 import com.onyx.android.sdk.data.OnyxDownloadManager;
@@ -8,9 +12,15 @@ import com.onyx.android.sdk.data.OnyxDownloadManager;
 /**
  * Created by suicheng on 2017/2/27.
  */
-public class MarketApplication extends Application {
+public class MarketApplication extends MultiDexApplication {
     static public MarketApplication sInstance;
     static private CloudStore cloudStore;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(MarketApplication.this);
+    }    
 
     @Override
     public void onCreate() {
