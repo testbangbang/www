227719edf56a21774149b6d91a1a9eaf95b9a2b0
wang.suicheng <wsc@onyx-international.com>
2017-06-01 15:48:41 +0800

    eschool: authAciton use tokenInterceptor for add common token header

--- android/kreader/app/edu/eschool/src/main/java/com/onyx/android/eschool/action/AuthTokenAction.java
+++ android/kreader/app/edu/eschool/src/main/java/com/onyx/android/eschool/action/AuthTokenAction.java
@@ -7,6 +7,7 @@ import com.onyx.android.eschool.events.AccountTokenErrorEvent;
 import com.onyx.android.eschool.holder.LibraryDataHolder;
 import com.onyx.android.sdk.common.request.BaseCallback;
 import com.onyx.android.sdk.common.request.BaseRequest;
+import com.onyx.android.sdk.data.Constant;
 import com.onyx.android.sdk.data.common.ContentException;
 import com.onyx.android.sdk.data.db.table.EduAccountProvider;
 import com.onyx.android.sdk.data.model.v2.BaseAuthAccount;
@@ -16,6 +17,8 @@ import com.onyx.android.sdk.data.request.cloud.CloudRequestChain;
 import com.onyx.android.sdk.data.request.cloud.v2.AccountLoadFromCloudRequest;
 import com.onyx.android.sdk.data.request.cloud.v2.AccountLoadFromLocalRequest;
 import com.onyx.android.sdk.data.request.cloud.v2.AccountSaveToLocalRequest;
+import com.onyx.android.sdk.data.v1.ServiceFactory;
+import com.onyx.android.sdk.data.v2.ContentService;
 import com.onyx.android.sdk.ui.utils.ToastUtils;
 import com.onyx.android.sdk.utils.FileUtils;
 import com.onyx.android.sdk.utils.NetworkUtil;
@@ -60,6 +63,7 @@ public class AuthTokenAction extends BaseAction<LibraryDataHolder> {
                 if (e != null) {
                     if (ContentException.isNetworkException(e)) {
                         if (NeoAccountBase.isValid(localAccountRequest.getAccount())) {
+                            updateRetrofit(dataHolder);
                             BaseCallback.invoke(baseCallback, request, null);
                             return;
                         }
@@ -76,6 +80,7 @@ public class AuthTokenAction extends BaseAction<LibraryDataHolder> {
                 if (e != null) {
                     return;
                 }
+                updateRetrofit(dataHolder);
                 sendAccountAvailableEvent();
                 BaseCallback.invoke(baseCallback, request, e);
             }
@@ -98,5 +103,11 @@ public class AuthTokenAction extends BaseAction<LibraryDataHolder> {
         }
         return BaseAuthAccount.create(FileUtils.computeMD5(macAddress + NAME_SECRET),
                 FileUtils.computeMD5(macAddress + PASSWORD_SECRET));
+    }
+
+    public static void updateRetrofit(LibraryDataHolder dataHolder) {
+        ServiceFactory.addRetrofitTokenHeader(dataHolder.getCloudManager().getCloudConf().getApiBase(),
+                Constant.HEADER_AUTHORIZATION,
+                ContentService.CONTENT_AUTH_PREFIX + dataHolder.getCloudManager().getToken());
     }
 }
