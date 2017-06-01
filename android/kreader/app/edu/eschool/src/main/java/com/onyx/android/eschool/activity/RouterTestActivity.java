4b7da61740052bc19f79ab5892b81eea2962a95a
john <zeng.zhu@gmail.com>
2017-06-01 17:28:03 +0800

    add more state control.

--- android/kreader/app/edu/eschool/src/main/java/com/onyx/android/eschool/activity/RouterTestActivity.java
+++ android/kreader/app/edu/eschool/src/main/java/com/onyx/android/eschool/activity/RouterTestActivity.java
@@ -1,6 +1,11 @@
 package com.onyx.android.eschool.activity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.app.Activity;
@@ -8,6 +13,7 @@ import android.util.Log;
 
 import com.onyx.android.eschool.R;
 import com.onyx.android.eschool.SchoolApp;
+import com.onyx.android.eschool.action.AuthTokenAction;
 import com.onyx.android.libsetting.data.wifi.AccessPoint;
 import com.onyx.android.libsetting.manager.WifiAdmin;
 import com.onyx.android.sdk.common.request.BaseCallback;
@@ -17,6 +23,7 @@ import com.onyx.android.sdk.utils.DeviceUtils;
 import com.onyx.android.sdk.utils.NetworkUtil;
 import com.umeng.analytics.MobclickAgent;
 
+import java.util.Calendar;
 import java.util.List;
 
 import de.halfbit.tinymachine.StateHandler;
@@ -26,19 +33,35 @@ import de.halfbit.tinymachine.StateHandler.Type;
 import static android.net.NetworkInfo.DetailedState.CONNECTED;
 
 public class RouterTestActivity extends Activity {
-    private static final int STATE_INIT = 0;
-    private static final int STATE_WIFI_ENABLED = 1;
-    private static final int STATE_WIFI_CONNECTED = 2;
-    private static final int STATE_NTP_SYNC = 3;
-    private static final int STATE_ALARM_SET = 4;
-    private static final int STATE_ALARM_TRIGGERED = 5;
-    private static final int STATE_ALARM_TESTING = 6;
+    private static final int STATE_INIT                         = 0;
+    private static final int STATE_WIFI_INIT_ENABLED            = 1;
+    private static final int STATE_WIFI_WAIT_FOR_SCAN_RESULT    = 2;
+    private static final int STATE_WIFI_SCAN_RESULT_READY       = 3;
+    private static final int STATE_WIFI_WAIT_FOR_CONNECTED      = 4;
+    private static final int STATE_WIFI_CONNECTED               = 5;
+    private static final int STATE_NTP_SYNC                     = 6;
+    private static final int STATE_ALARM_SET                    = 7;
+    private static final int STATE_ALARM_TRIGGERED              = 8;
+    private static final int STATE_WAIT_FOR_ENABLE_AFTER_ALARM  = 9;
+    private static final int STATE_WIFI_ENABLED_AFTER_ALARM     = 10;
+    private static final int STATE_WAIT_FOR_CONNECTED_AFTER_ALARM = 11;
+    private static final int STATE_CONNECTED_AFTER_ALARM        = 12;
+    private static final int STATE_START_TESTING                = 8;
+    private static final int STATE_CLOUD_AUTH                   = 9;
+    private static final int STATE_CLOUD_DOWNLOAD               = 10;
 
     private static final String TAG = RouterTestActivity.class.getSimpleName();
     private TinyMachine tinyMachine = new TinyMachine(new RouterStateHandler(), STATE_INIT);
     private WifiAdmin wifiAdmin;
-    private static final String ONYX_WIFI_TEST_ROUTER_SSID = "onyx-edu-test";
+    private static final String ONYX_WIFI_TEST_ROUTER_SSID = "onyx-zeng";
     private static final String ONYX_WIFI_TEST_ROUTER_PW = "OnyxWpa2009";
+
+    private static int ALARM_START_INTERVAL_TIME = 1;
+    private static long ALARM_REPEAT_INTERVAL_TIME = 60 * 1000;
+    private static String ALARM_INTENT_ACTION = "com.action.router.AlarmManager";
+    private BroadcastReceiver alarmReceiver;
+
+    private AccessPoint lastAccessPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,14 +110,21 @@ public class RouterTestActivity extends Activity {
             @Override
             public void onWifiStateChange(boolean isWifiEnable) {
                 if (isWifiEnable && tinyMachine.getCurrentState() == STATE_INIT) {
-                    tinyMachine.transitionTo(STATE_WIFI_ENABLED);
+                    tinyMachine.transitionTo(STATE_WIFI_INIT_ENABLED);
+                }
+                if (isWifiEnable && tinyMachine.getCurrentState() == STATE_WAIT_FOR_ENABLE_AFTER_ALARM) {
+                    tinyMachine.transitionTo(STATE_WIFI_ENABLED_AFTER_ALARM);
                 }
             }
 
             @Override
             public void onScanResultReady(List<AccessPoint> scanResult) {
-                if (tinyMachine.getCurrentState() == STATE_WIFI_ENABLED) {
+                if (tinyMachine.getCurrentState() == STATE_WIFI_WAIT_FOR_SCAN_RESULT) {
+                    tinyMachine.transitionTo(STATE_WIFI_SCAN_RESULT_READY);
                     actionOnWiFiScanResultReady(scanResult);
+                }
+                if (tinyMachine.getCurrentState() == STATE_ALARM_TRIGGERED) {
+
                 }
             }
 
@@ -105,11 +135,26 @@ public class RouterTestActivity extends Activity {
 
             @Override
             public void onNetworkConnectionChange(NetworkInfo.DetailedState state) {
-                if (state == CONNECTED && tinyMachine.getCurrentState() == STATE_WIFI_ENABLED) {
+                if (state == CONNECTED && tinyMachine.getCurrentState() == STATE_WIFI_WAIT_FOR_CONNECTED) {
                     tinyMachine.transitionTo(STATE_WIFI_CONNECTED);
+                }
+                if (state == CONNECTED && tinyMachine.getCurrentState() == STATE_WAIT_FOR_CONNECTED_AFTER_ALARM) {
+                    tinyMachine.transitionTo(STATE_CONNECTED_AFTER_ALARM);
                 }
             }
         });
+
+        alarmReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(ALARM_INTENT_ACTION)) {
+                    onReceivedAlarm();
+                }
+            }
+        };
+        final IntentFilter alarmFilter = new IntentFilter();
+        alarmFilter.addAction(ALARM_INTENT_ACTION);
+        registerReceiver(alarmReceiver, alarmFilter);
     }
 
     private void triggerInitEvent() {
@@ -121,7 +166,8 @@ public class RouterTestActivity extends Activity {
         NetworkUtil.enableWiFi(this, true);
     }
 
-    public void actionOnWifiEnabled() {
+    public void actionOnWifiInitEnabled() {
+        tinyMachine.transitionTo(STATE_WIFI_WAIT_FOR_SCAN_RESULT);
         getWifiAdmin().triggerWifiScan();
     }
 
@@ -129,8 +175,10 @@ public class RouterTestActivity extends Activity {
         for (AccessPoint accessPoint : scanResultList) {
             Log.e(TAG, accessPoint.getScanResult().SSID);
             if (accessPoint.getScanResult().SSID.equalsIgnoreCase(ONYX_WIFI_TEST_ROUTER_SSID)) {
+                lastAccessPoint = accessPoint;
                 accessPoint.setPassword(ONYX_WIFI_TEST_ROUTER_PW);
-                wifiAdmin.connectWifi(accessPoint);
+                getWifiAdmin().connectWifi(accessPoint);
+                tinyMachine.transitionTo(STATE_WIFI_WAIT_FOR_CONNECTED);
                 Log.e(TAG, "Connecting to AP: " + accessPoint.getScanResult().SSID);
                 return;
             }
@@ -149,32 +197,124 @@ public class RouterTestActivity extends Activity {
     }
 
     public void actionOnSntpSync() {
+        triggerRepeatedAlarm();
+        NetworkUtil.enableWiFi(this, false);
+        tinyMachine.transitionTo(STATE_ALARM_SET);
+    }
 
+    public void actionOnAlarmTriggered() {
+        tinyMachine.transitionTo(STATE_WAIT_FOR_ENABLE_AFTER_ALARM);
+        NetworkUtil.enableWiFi(this, true);
+    }
+
+    public void actionOnWaitForWiFiEnableAfterAlarm() {
+
+    }
+
+    public void actionOnWiFiEnabledAfterAlarm() {
+        getWifiAdmin().connectWifi(lastAccessPoint);
+        tinyMachine.transitionTo(STATE_WAIT_FOR_CONNECTED_AFTER_ALARM);
+    }
+
+    public void actionOnConnectedAfterAlarm() {
+        final AuthTokenAction authTokenAction = new AuthTokenAction();
+        authTokenAction.execute(SchoolApp.getLibraryDataHolder(), new BaseCallback() {
+            @Override
+            public void done(BaseRequest request, Throwable e) {
+
+            }
+        });
     }
 
     // Define state handler class with handler methods
     public class RouterStateHandler {
-
         @StateHandler(state = STATE_INIT)
         public void onEventStateInit(String event, TinyMachine tm) {
+            Log.e(TAG, "state changed to STATE_INIT");
             actionOnInitState();
         }
 
-        @StateHandler(state = STATE_WIFI_ENABLED, type = Type.OnEntry)
-        public void onEntryStateWifiEnabled() {
-            actionOnWifiEnabled();
+        @StateHandler(state = STATE_WIFI_INIT_ENABLED, type = Type.OnEntry)
+        public void onEntryStateWifiInitEnabled() {
+            Log.e(TAG, "state changed to STATE_WIFI_INIT_ENABLED");
+            actionOnWifiInitEnabled();
+        }
+
+        @StateHandler(state = STATE_WIFI_SCAN_RESULT_READY, type = Type.OnEntry)
+        public void onEntryStateScanResultReady() {
+            Log.e(TAG, "state changed to STATE_WIFI_SCAN_RESULT_READY");
         }
 
         @StateHandler(state = STATE_WIFI_CONNECTED, type = Type.OnEntry)
         public void onEntryStateWifiConnected() {
+            Log.e(TAG, "state changed to STATE_WIFI_CONNECTED");
             actionOnWifiConnected();
         }
 
         @StateHandler(state = STATE_NTP_SYNC, type = Type.OnEntry)
         public void onEntryStateSntpSync() {
+            Log.e(TAG, "state changed to STATE_NTP_SYNC");
             actionOnSntpSync();
         }
 
+        @StateHandler(state = STATE_ALARM_SET, type = Type.OnEntry)
+        public void onEntryStateAlarmSet() {
+            Log.e(TAG, "state changed to STATE_ALARM_SET");
+            actionOnSntpSync();
+        }
+
+        @StateHandler(state = STATE_ALARM_TRIGGERED, type = Type.OnEntry)
+        public void onEntryStateAlarmTriggered() {
+            Log.e(TAG, "state changed to STATE_ALARM_TRIGGERED");
+            actionOnAlarmTriggered();
+        }
+
+        @StateHandler(state = STATE_WIFI_ENABLED_AFTER_ALARM, type = Type.OnEntry)
+        public void onEntryStateWiFiEnabledAfterAlarm() {
+            Log.e(TAG, "state changed to STATE_WIFI_ENABLED_AFTER_ALARM");
+            actionOnWiFiEnabledAfterAlarm();
+        }
+
+        @StateHandler(state = STATE_CONNECTED_AFTER_ALARM, type = Type.OnEntry)
+        public void onEntryStateConnectedAfterAlarm() {
+            Log.e(TAG, "state changed to STATE_CONNECTED_AFTER_ALARM");
+            actionOnConnectedAfterAlarm();
+        }
+
+        @StateHandler(state = STATE_WAIT_FOR_ENABLE_AFTER_ALARM, type = Type.OnEntry)
+        public void onEntryStateWaitForEnableAfterAlarm() {
+            Log.e(TAG, "state changed to STATE_WAIT_FOR_ENABLE_AFTER_ALARM");
+        }
+
+
+    }
+
+    private void triggerRepeatedAlarm() {
+        Calendar c = Calendar.getInstance();
+        int minute = c.get(Calendar.MINUTE);
+        minute += ALARM_START_INTERVAL_TIME;
+        if (minute >= 60) {
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            c.set(Calendar.HOUR_OF_DAY, hour + 1);
+            minute %= 60;
+        }
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        am.setTimeZone("Asia/Hong_Kong");
+        am.setRepeating(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), ALARM_REPEAT_INTERVAL_TIME, getAlarmPendingIntent());
+    }
+
+    private PendingIntent getAlarmPendingIntent() {
+        Intent intent = new Intent(ALARM_INTENT_ACTION);
+        PendingIntent startAlarmPendingIntent = PendingIntent.getBroadcast(RouterTestActivity.this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        return startAlarmPendingIntent;
+    }
+
+    private void onReceivedAlarm() {
+        tinyMachine.transitionTo(STATE_ALARM_TRIGGERED);
     }
 
 }
