57aced3aa25d450d565c245f0a7388ccc26f7803
wang.suicheng <wsc@onyx-international.com>
2017-06-04 15:31:39 +0800

    eschool: add btn to read user manual doc

--- android/kreader/app/eschool/src/main/java/com/onyx/android/eschool/activity/DeviceParamsActivity.java
+++ android/kreader/app/eschool/src/main/java/com/onyx/android/eschool/activity/DeviceParamsActivity.java
@@ -1,11 +1,24 @@
 package com.onyx.android.eschool.activity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+
 import com.onyx.android.eschool.R;
+import com.onyx.android.sdk.ui.utils.ToastUtils;
+import com.onyx.android.sdk.utils.ActivityUtil;
+import com.onyx.android.sdk.utils.ViewDocumentUtils;
+
+import java.io.File;
+
+import butterknife.OnClick;
 
 /**
  * Created by suicheng on 2017/6/3.
  */
 public class DeviceParamsActivity extends BaseActivity {
+
+    private String userManualFilePath = "/mnt/sdcard/slide/user_manual.pdf";
+
     @Override
     protected Integer getLayoutId() {
         return R.layout.activity_device_params;
@@ -21,6 +34,24 @@ public class DeviceParamsActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+    }
 
+    @OnClick(R.id.manual_guide_bt)
+    void onManualGuideClick() {
+        ActivityUtil.startActivitySafely(this, getUserManualIntent());
+    }
+
+    private Intent getUserManualIntent() {
+        File file = new File(userManualFilePath);
+        if (!file.exists()) {
+            ToastUtils.showToast(this, "用户手册文档被删除了，无法打开");
+            return null;
+        }
+        Intent intent = ViewDocumentUtils.viewActionIntentWithMimeType(file);
+        ComponentName component = ViewDocumentUtils.getReaderComponentName(this);
+        if (component != null) {
+            intent.setComponent(component);
+        }
+        return intent;
     }
 }
