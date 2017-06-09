441909cca4942e2c251f66a56b25be3bda65f503
梁小明 <351062367@qq.com>
2017-06-09 19:27:00 +0800

    add load eduaccount for save documentdata to cloud

--- android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/request/cloud/SaveDocumentDataToCloudRequest.java
+++ android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/request/cloud/SaveDocumentDataToCloudRequest.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.onyx.android.sdk.data.CloudManager;
 import com.onyx.android.sdk.data.Constant;
 import com.onyx.android.sdk.data.model.JsonRespone;
+import com.onyx.android.sdk.data.request.cloud.v2.AccountLoadFromLocalRequest;
 import com.onyx.android.sdk.data.v1.ServiceFactory;
 import com.onyx.android.sdk.data.v2.ContentService;
 import com.onyx.android.sdk.utils.FileUtils;
@@ -29,18 +30,20 @@ public class SaveDocumentDataToCloudRequest extends BaseCloudRequest {
     private String url;
     private String fileFullMd5;
     private String docId;
-    private String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTMyMzBhZDY4Njg3MWViMDY1YWUxN2IiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE0OTY5NzgxNjEsImV4cCI6MTQ5OTU3MDE2MX0.J5p5ttj-IgFNNI3pdJCNtflCg36AFzbr6_R3XB9PeOo";
+    private String token;
 
     public SaveDocumentDataToCloudRequest(String uploadDBPath,
                                           Context context,
                                           String url,
                                           String fileFullMd5,
-                                          String docId) {
+                                          String docId,
+                                          String token) {
         this.uploadDBPath = uploadDBPath;
         this.context = context;
         this.url = url;
         this.fileFullMd5 = fileFullMd5;
         this.docId = docId;
+        this.token = token;
     }
 
     @Override
