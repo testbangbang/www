db6c626c46bcabcbffdd13583901255156c7ed73
wang.suicheng <wsc@onyx-international.com>
2017-06-22 22:32:06 +0800

    eschool: use indexService in the authTokenAction

--- android/kreader/app/edu/eschool/src/main/java/com/onyx/android/eschool/action/AuthTokenAction.java
+++ android/kreader/app/edu/eschool/src/main/java/com/onyx/android/eschool/action/AuthTokenAction.java
@@ -4,18 +4,27 @@ import android.content.Context;
 
 import com.onyx.android.eschool.R;
 import com.onyx.android.eschool.SchoolApp;
+import com.onyx.android.eschool.device.DeviceConfig;
 import com.onyx.android.eschool.events.AccountAvailableEvent;
 import com.onyx.android.eschool.events.AccountTokenErrorEvent;
 import com.onyx.android.eschool.events.HardwareErrorEvent;
 import com.onyx.android.eschool.holder.LibraryDataHolder;
+import com.onyx.android.eschool.manager.LeanCloudManager;
 import com.onyx.android.sdk.common.request.BaseCallback;
 import com.onyx.android.sdk.common.request.BaseRequest;
+import com.onyx.android.sdk.data.CloudManager;
+import com.onyx.android.sdk.data.Constant;
 import com.onyx.android.sdk.data.db.table.EduAccountProvider;
+import com.onyx.android.sdk.data.model.v2.IndexService;
 import com.onyx.android.sdk.data.model.v2.BaseAuthAccount;
 import com.onyx.android.sdk.data.model.v2.EduAccount;
 import com.onyx.android.sdk.data.model.v2.NeoAccountBase;
+import com.onyx.android.sdk.data.request.cloud.CloudRequestChain;
+import com.onyx.android.sdk.data.request.cloud.v2.CloudIndexServiceRequest;
 import com.onyx.android.sdk.data.request.cloud.v2.LoginByHardwareInfoRequest;
 import com.onyx.android.sdk.data.request.cloud.v2.GenerateAccountInfoRequest;
+import com.onyx.android.sdk.data.utils.CloudConf;
+import com.onyx.android.sdk.utils.NetworkUtil;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -36,9 +45,15 @@ public class AuthTokenAction extends BaseAction<LibraryDataHolder> {
             sendHardwareErrorEvent();
             return;
         }
+        setMainIndexServerCloudConf(dataHolder.getContext(), dataHolder.getCloudManager());
+        final CloudRequestChain requestChain = new CloudRequestChain();
+        final CloudIndexServiceRequest authServiceRequest = new CloudIndexServiceRequest(createAuthService(dataHolder.getContext()));
         final LoginByHardwareInfoRequest accountLoadRequest = new LoginByHardwareInfoRequest<>(EduAccountProvider.CONTENT_URI, EduAccount.class);
+        authServiceRequest.setLocalLoadRetryCount(localLoadRetryCount);
         accountLoadRequest.setLocalLoadRetryCount(localLoadRetryCount);
-        dataHolder.getCloudManager().submitRequest(dataHolder.getContext(), accountLoadRequest, new BaseCallback() {
+        requestChain.setAbortException(false);
+        requestChain.addRequest(authServiceRequest, null);
+        requestChain.addRequest(accountLoadRequest, new BaseCallback() {
             @Override
             public void done(BaseRequest request, Throwable e) {
                 if (e != null) {
@@ -54,6 +69,7 @@ public class AuthTokenAction extends BaseAction<LibraryDataHolder> {
                 }
             }
         });
+        requestChain.execute(dataHolder.getContext(), dataHolder.getCloudManager());
     }
 
     private void processCloudException(Context context, Throwable e) {
@@ -88,5 +104,19 @@ public class AuthTokenAction extends BaseAction<LibraryDataHolder> {
 
     public void setLocalLoadRetryCount(int retryCount) {
         this.localLoadRetryCount = retryCount;
+    }
+
+    private IndexService createAuthService(Context context) {
+        IndexService authService = new IndexService();
+        authService.mac = NetworkUtil.getMacAddress(context);
+        authService.installationId = LeanCloudManager.getInstallationId();
+        return authService;
+    }
+
+    public static void setMainIndexServerCloudConf(Context context, CloudManager cloudManager) {
+        cloudManager.setAllCloudConf(CloudConf.create(
+                DeviceConfig.sharedInstance(context).getCloudMainIndexServerHost(),
+                DeviceConfig.sharedInstance(context).getCloudMainIndexServerApi(),
+                Constant.DEFAULT_CLOUD_STORAGE));
     }
 }
