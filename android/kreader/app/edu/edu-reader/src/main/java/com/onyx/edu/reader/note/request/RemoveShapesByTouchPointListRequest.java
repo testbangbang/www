2ff359e2a83683dc8746c83f995e1f6071057664
john <john@onyx-international.com>
2017-07-01 12:27:59 +0800

    fix shape removed issue. when shape removed, make sure the content render flag set to true.

--- android/kreader/app/edu/edu-reader/src/main/java/com/onyx/edu/reader/note/request/RemoveShapesByTouchPointListRequest.java
+++ android/kreader/app/edu/edu-reader/src/main/java/com/onyx/edu/reader/note/request/RemoveShapesByTouchPointListRequest.java
@@ -1,5 +1,7 @@
 package com.onyx.edu.reader.note.request;
 
+import android.util.Log;
+
 import com.onyx.android.sdk.data.PageInfo;
 import com.onyx.android.sdk.scribble.data.TouchPointList;
 import com.onyx.edu.reader.note.NoteManager;
@@ -21,13 +23,17 @@ public class RemoveShapesByTouchPointListRequest extends ReaderBaseNoteRequest {
     }
 
     public void execute(final NoteManager noteManager) throws Exception {
+        benchmarkStart();
         final PageInfo pageInfo = getVisiblePages().get(0);
         final float radius = noteManager.getNoteDrawingArgs().eraserRadius / pageInfo.getActualScale();
         final ReaderNotePage notePage = noteManager.getNoteDocument().loadPage(getContext(), pageInfo.getName(), 0);
+        boolean changed = false;
         if (notePage != null) {
-            notePage.removeShapesByTouchPointList(touchPointList, radius);
+            changed |= notePage.removeShapesByTouchPointList(touchPointList, radius);
         }
-        getNoteDataInfo().setContentRendered(renderVisiblePages(noteManager));
+        changed |= renderVisiblePages(noteManager);
+        getNoteDataInfo().setContentRendered(changed);
         setResumeRawInputProcessor(noteManager.isDFBForCurrentShape());
+        Log.e("############", "erase takes: " + benchmarkEnd() + " changed: " + changed);
     }
 }
