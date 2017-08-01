98d323ba747ed3cf425679aaf0a59b8b7982eabf
hehai <hehai@onyx-international.com>
2017-08-01 19:42:44 +0800

    clean code.

--- android/kreader/app/oem/dr/src/main/java/com/onyx/android/dr/util/RegularUtil.java
+++ android/kreader/app/oem/dr/src/main/java/com/onyx/android/dr/util/RegularUtil.java
@@ -8,132 +8,132 @@ import java.util.regex.Pattern;
 
 public class RegularUtil {
     /**
-     * 正则表达式：验证用户名
+     * regex username
      */
     public static final String REGEX_USERNAME = "^[a-zA-Z]\\w{5,17}$";
 
     /**
-     * 正则表达式：验证密码
+     * regex password
      */
     public static final String REGEX_PASSWORD = "^[a-zA-Z0-9]{6,16}$";
 
     /**
-     * 移动手机号码的正则表达式。
+     * regex china mobile
      */
     private static final String REGEX_CHINA_MOBILE ="1(3[4-9]|4[7]|5[012789]|8[278])\\d{8}";
 
     /**
-     * 联通手机号码的正则表达式。
+     * regex china unicom
      */
     private static final String REGEX_CHINA_UNICOM = "1(3[0-2]|5[56]|8[56])\\d{8}";
 
     /**
-     * 电信手机号码的正则表达式。
+     * regex china telecom
      */
     private static final String REGEX_CHINA_TELECOM = "(?!00|015|013)(0\\d{9,11})|(1(33|53|80|89)\\d{8})";
 
     /**
-     * 正则表达式：验证手机号
+     * regex phone number
      */
     private static final String REGEX_PHONE_NUMBER = "^(0(10|2\\d|[3-9]\\d\\d)[- ]{0,3}\\d{7,8}|0?1[3584]\\d{9})$";
 
     /**
-     * 正则表达式：验证邮箱
+     * regex email
      */
     public static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
 
     /**
-     * 正则表达式：验证汉字
+     * regex Chinese char
      */
     public static final String REGEX_CHINESE = "^[\u4e00-\u9fa5],{0,}$";
 
     /**
-     * 正则表达式：验证身份证
+     * regex id card
      */
     public static final String REGEX_ID_CARD = "(^\\d{18}$)|(^\\d{15}$)";
 
     /**
-     * 正则表达式：验证URL
+     * regex URL
      */
     public static final String REGEX_URL = "http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?";
 
     /**
-     * 正则表达式：验证IP地址
+     * regex ip
      */
     public static final String REGEX_IP_ADDR = "(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)";
 
     /**
-     * 校验用户名
+     * Check the username
      *
      * @param username
-     * @return 校验通过返回true，否则返回false
+     * @return
      */
     public static boolean isUsername(String username) {
         return Pattern.matches(REGEX_USERNAME, username);
     }
 
     /**
-     * 校验密码
+     * Check the password
      *
      * @param password
-     * @return 校验通过返回true，否则返回false
+     * @return
      */
     public static boolean isPassword(String password) {
         return Pattern.matches(REGEX_PASSWORD, password);
     }
 
     /**
-     * 校验手机号
+     * Check the phone number
      *
      * @param mobile
-     * @return 校验通过返回true，否则返回false
+     * @return
      */
     public static boolean isMobile(String mobile) {
         return Pattern.matches(REGEX_PHONE_NUMBER, mobile);
     }
 
     /**
-     * 校验邮箱
+     * Check the mailbox
      *
      * @param email
-     * @return 校验通过返回true，否则返回false
+     * @return
      */
     public static boolean isEmail(String email) {
         return Pattern.matches(REGEX_EMAIL, email);
     }
 
     /**
-     * 校验汉字
+     * Check Chinese characters
      *
      * @param chinese
-     * @return 校验通过返回true，否则返回false
+     * @return
      */
     public static boolean isChinese(String chinese) {
         return Pattern.matches(REGEX_CHINESE, chinese);
     }
 
     /**
-     * 校验身份证
+     * Check the identity card
      *
      * @param idCard
-     * @return 校验通过返回true，否则返回false
+     * @return
      */
     public static boolean isIDCard(String idCard) {
         return Pattern.matches(REGEX_ID_CARD, idCard);
     }
 
     /**
-     * 校验URL
+     * Check url
      *
      * @param url
-     * @return 校验通过返回true，否则返回false
+     * @return
      */
     public static boolean isUrl(String url) {
         return Pattern.matches(REGEX_URL, url);
     }
 
     /**
-     * 校验IP地址
+     * Check ip
      *
      * @param ipAddr
      * @return
