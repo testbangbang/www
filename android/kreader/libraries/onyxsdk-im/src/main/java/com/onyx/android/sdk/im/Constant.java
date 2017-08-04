f054bca4c2be3c462272216f673b624af7d80d21
wang.suicheng <wsc@onyx-international.com>
2017-08-04 20:19:10 +0800

    onyxsdk-im: Message add type field

--- android/kreader/libraries/onyxsdk-im/src/main/java/com/onyx/android/sdk/im/Constant.java
+++ android/kreader/libraries/onyxsdk-im/src/main/java/com/onyx/android/sdk/im/Constant.java
@@ -9,9 +9,10 @@ public class Constant {
     public final static String PUBLIC_CHANNEL = "public";
     public final static String NEW_MESSAGE = "new message";
 
-    public final static String MESSAGE_ID = "msg_id";
+    public final static String MESSAGE_ID = "id";
     public final static String MESSAGE_CONTENT = "content";
     public final static String MESSAGE_ACTION = "action";
+    public final static String MESSAGE_TYPE = "type";
 
     public final static String AVOSCLOUD_CHANNEL = "com.avos.avoscloud.Channel";
     public final static String AVOSCLOUD_DATA = "com.avos.avoscloud.Data";
