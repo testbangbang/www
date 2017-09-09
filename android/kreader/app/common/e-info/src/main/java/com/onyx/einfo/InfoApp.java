11bc1d0ebb6a69887bd38f772b1b58bf3c1f023a
wang.suicheng <wsc@onyx-international.com>
2017-09-09 11:27:43 +0800

    e-info: add the func of content importing from tf card

--- android/kreader/app/common/e-info/src/main/java/com/onyx/einfo/InfoApp.java
+++ android/kreader/app/common/e-info/src/main/java/com/onyx/einfo/InfoApp.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
+import android.util.Log;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.onyx.android.sdk.common.request.BaseCallback;
+import com.onyx.android.sdk.common.request.BaseRequest;
 import com.onyx.android.sdk.data.action.ActionContext;
 import com.onyx.android.sdk.data.utils.JSONObjectParseUtils;
 import com.onyx.android.sdk.im.IMConfig;
@@ -37,6 +40,7 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -45,6 +49,8 @@ import java.util.Map;
  * Created by zhuzeng on 14/11/2016.
  */
 public class InfoApp extends MultiDexApplication {
+    private static final String TAG = "InfoApp";
+
     public static boolean checkedOnBootComplete = false;
     static private final String MMC_STORAGE_ID = "flash";
 
@@ -114,15 +120,13 @@ public class InfoApp extends MultiDexApplication {
 
     private void initEventListener() {
         deviceReceiver.setMediaStateListener(new DeviceReceiver.MediaStateListener() {
+
             @Override
             public void onMediaMounted(Intent intent) {
+                Log.w(TAG, "onMediaMounted " + intent.getData().toString());
                 if (EnvironmentUtil.isRemovableSDDirectory(getApplicationContext(), intent)) {
-                    String sdcardCid = EnvironmentUtil.getRemovableSDCardCid();
-                    if (StringUtils.isNullOrEmpty(sdcardCid)) {
-                        return;
-                    }
-                    FileSystemScanAction systemScanAction = new FileSystemScanAction(sdcardCid, false);
-                    systemScanAction.execute(getLibraryDataHolder(), null);
+                    //processRemovableSDCardScan();
+                    processRemovableSDCardContentImport();
                 }
             }
 
@@ -150,6 +154,28 @@ public class InfoApp extends MultiDexApplication {
             }
         });
         deviceReceiver.enable(getApplicationContext(), true);
+    }
+
+    private void processRemovableSDCardScan() {
+        String sdcardCid = EnvironmentUtil.getRemovableSDCardCid();
+        if (StringUtils.isNullOrEmpty(sdcardCid)) {
+            return;
+        }
+        FileSystemScanAction systemScanAction = new FileSystemScanAction(sdcardCid, false);
+        systemScanAction.execute(getLibraryDataHolder(), null);
+    }
+
+    private void processRemovableSDCardContentImport() {
+        String jsonFilePath = DeviceConfig.sharedInstance(getApplicationContext()).getCloudContentImportJsonFilePath();
+        if (StringUtils.isNullOrEmpty(jsonFilePath)) {
+            return;
+        }
+        final File file = new File(EnvironmentUtil.getRemovableSDCardDirectory(), jsonFilePath);
+        if (!file.exists()) {
+            return;
+        }
+        ContentImportAction importAction = new ContentImportAction(file.getAbsolutePath(), true);
+        importAction.execute(getLibraryDataHolder(), null);
     }
 
     public void turnOffLed() {
@@ -236,10 +262,18 @@ public class InfoApp extends MultiDexApplication {
     }
 
     private void cloudContentImportFirstBoot() {
-        if (checkedOnBootComplete) {
+        if (checkedOnBootComplete || ConfigPreferenceManager.hasImportContent(this)) {
             return;
         }
-        ContentImportAction importAction = new ContentImportAction();
-        importAction.execute(InfoApp.getLibraryDataHolder(), null);
+        String jsonFilePath = DeviceConfig.sharedInstance(getApplicationContext()).getCloudContentImportJsonFilePath();
+        if (StringUtils.isNullOrEmpty(jsonFilePath)) {
+            return;
+        }
+        File file = new File(EnvironmentUtil.getExternalStorageDirectory(), jsonFilePath);
+        if (!file.exists()) {
+            return;
+        }
+        ContentImportAction importAction = new ContentImportAction(file.getAbsolutePath());
+        importAction.execute(getLibraryDataHolder(), null);
     }
 }
