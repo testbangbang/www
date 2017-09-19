077bea4db93c6341fe3a7b0bfcc441f66ed85826
wang.suicheng <wsc@onyx-international.com>
2017-09-20 00:00:46 +0800

    onyxsdk-data: sync the fileds of Metadata/Library/EduAccount

--- android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/model/v2/NeoAccountBase.java
+++ android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/model/v2/NeoAccountBase.java
@@ -3,10 +3,12 @@ package com.onyx.android.sdk.data.model.v2;
 import com.alibaba.fastjson.annotation.JSONField;
 import com.onyx.android.sdk.data.converter.ListStringConverter;
 import com.onyx.android.sdk.data.model.BaseData;
+import com.onyx.android.sdk.data.model.Device;
 import com.onyx.android.sdk.data.utils.JSONObjectParseUtils;
 import com.onyx.android.sdk.utils.CollectionUtils;
 import com.onyx.android.sdk.utils.StringUtils;
 import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.ColumnIgnore;
 
 import java.util.Date;
 import java.util.List;
@@ -18,6 +20,7 @@ public class NeoAccountBase extends BaseData {
 
     public static final String DELIMITER = ",";
 
+    public String _id;
     public String name;
     public String orgName;
     @Column(typeConverter = ListStringConverter.class)
@@ -31,6 +34,8 @@ public class NeoAccountBase extends BaseData {
 
     public String info;
     public String library;
+    @ColumnIgnore
+    public List<Device> devices;
 
     @JSONField(serialize = false, deserialize = false)
     public String getFirstGroup() {
@@ -38,6 +43,14 @@ public class NeoAccountBase extends BaseData {
             return "";
         }
         return groups.get(0).replaceAll(DELIMITER, "");
+    }
+
+    @JSONField(serialize = false, deserialize = false)
+    public Device getFirstDevice() {
+        if(CollectionUtils.isNullOrEmpty(devices)) {
+            return null;
+        }
+        return devices.get(0);
     }
 
     public String getPhone() {
@@ -61,6 +74,7 @@ public class NeoAccountBase extends BaseData {
         return getCreatedAt().getTime() + getTokenExpiresIn() < new Date().getTime();
     }
 
+    @JSONField(serialize = false, deserialize = false)
     public long getTokenExpiresIn() {
         return tokenExpiresIn * 1000;
     }
@@ -72,5 +86,23 @@ public class NeoAccountBase extends BaseData {
                 account.name = common.name;
             }
         }
+    }
+
+    public static void parseInfo(NeoAccountBase account) {
+        if (account != null) {
+            AccountCommon common = JSONObjectParseUtils.parseObject(account.info, AccountCommon.class);
+            if (common != null) {
+                account.name = common.name;
+                account.orgName = common.organization;
+                if (StringUtils.isNotBlank(common.phone)) {
+                    account.phone = common.phone;
+                }
+            }
+        }
+    }
+
+    public void setAuthToken(String token, long tokenExpiresIn) {
+        this.token = token;
+        this.tokenExpiresIn = tokenExpiresIn;
     }
 }
