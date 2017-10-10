6922b06529a958864c0f6ddfa1253344b258b6dd
john <john@onyx-international.com>
2017-10-10 22:08:34 +0800

    add save points.

--- android/kreader/app/sandbox/sample/src/main/java/com/onyx/android/sample/activity/ScribbleTouchScreenDemoActivity.java
+++ android/kreader/app/sandbox/sample/src/main/java/com/onyx/android/sample/activity/ScribbleTouchScreenDemoActivity.java
@@ -1,10 +1,15 @@
 package com.onyx.android.sample.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -14,29 +19,47 @@ import android.widget.Button;
 import com.onyx.android.sample.R;
 import com.onyx.android.sample.device.DeviceConfig;
 import com.onyx.android.sdk.api.device.epd.EpdController;
+import com.onyx.android.sdk.scribble.data.TouchPoint;
+import com.onyx.android.sdk.utils.FileUtils;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ScribbleTouchScreenDemoActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int PEN_STOP = 0;
+    private static final int PEN_START = 1;
+    private static final int PEN_DRAWING = 2;
+    private static final int PEN_PAUSE = 3;
+
     @Bind(R.id.button_pen)
     Button buttonPen;
     @Bind(R.id.button_eraser)
     Button buttonEraser;
+    @Bind(R.id.button_save)
+    Button buttonSave;
+
     @Bind(R.id.surfaceview)
     SurfaceView surfaceView;
 
     boolean scribbleMode = false;
+    List<TouchPoint> points = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scribble_touch_screen_demo);
 
+        verifyStoragePermissions(this);
+
         ButterKnife.bind(this);
         buttonPen.setOnClickListener(this);
         buttonEraser.setOnClickListener(this);
+        buttonSave.setOnClickListener(this);
 
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
@@ -58,10 +81,6 @@ public class ScribbleTouchScreenDemoActivity extends AppCompatActivity implement
         surfaceView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent e) {
-                if (!scribbleMode) {
-                    return false;
-                }
-
                 // ignore multi touch
                 if (e.getPointerCount() > 1) {
                     return false;
@@ -71,24 +90,20 @@ public class ScribbleTouchScreenDemoActivity extends AppCompatActivity implement
 
                 switch (e.getAction() & MotionEvent.ACTION_MASK) {
                     case (MotionEvent.ACTION_DOWN):
-                        float dst[] = mapPoint(e.getX(), e.getY());
-                        EpdController.startStroke(baseWidth, dst[0], dst[1], e.getPressure(), e.getSize(), e.getEventTime());
+                        points.add(new TouchPoint(e));
                         return true;
                     case (MotionEvent.ACTION_CANCEL):
                     case (MotionEvent.ACTION_OUTSIDE):
                         break;
                     case MotionEvent.ACTION_UP:
-                        dst = mapPoint(e.getX(), e.getY());
-                        EpdController.finishStroke(baseWidth, dst[0], dst[1], e.getPressure(), e.getSize(), e.getEventTime());
+                        points.add(new TouchPoint(e));
                         return true;
                     case MotionEvent.ACTION_MOVE:
                         int n = e.getHistorySize();
-                        for (int i = 0; i < n; i++) {
-                            dst = mapPoint(e.getHistoricalX(i), e.getHistoricalY(i));
-                            EpdController.addStrokePoint(baseWidth,  dst[0], dst[1], e.getPressure(), e.getSize(), e.getEventTime());
+                        for(int i = 0; i < n; ++i) {
+                            points.add(TouchPoint.fromHistorical(e, i));
                         }
-                        dst = mapPoint(e.getX(), e.getY());
-                        EpdController.addStrokePoint(baseWidth, dst[0], dst[1], e.getPressure(), e.getSize(), e.getEventTime());
+                        points.add(new TouchPoint(e));
                         return true;
                     default:
                         break;
@@ -119,6 +134,9 @@ public class ScribbleTouchScreenDemoActivity extends AppCompatActivity implement
             leaveScribbleMode();
             cleanSurfaceView();
             return;
+        } else if (v.equals(buttonSave)) {
+            leaveScribbleMode();
+            savePoints();
         }
     }
 
@@ -135,30 +153,58 @@ public class ScribbleTouchScreenDemoActivity extends AppCompatActivity implement
     }
 
     private void enterScribbleMode() {
-        EpdController.enterScribbleMode(surfaceView);
         scribbleMode = true;
+        EpdController.setStrokeWidth(3.0f);
+        EpdController.setScreenHandWritingPenState(surfaceView, PEN_DRAWING);
     }
 
     private void leaveScribbleMode() {
         scribbleMode = false;
-        EpdController.leaveScribbleMode(surfaceView);
+        EpdController.setScreenHandWritingPenState(surfaceView, PEN_STOP);
     }
 
-    private float[] mapPoint(float x, float y) {
-        x = Math.min(Math.max(0, x), surfaceView.getWidth());
-        y = Math.min(Math.max(0, y), surfaceView.getHeight());
+    private void savePoints() {
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter("/mnt/sdcard/points.txt");
+            String newLine = System.getProperty("line.separator");
+            for(TouchPoint touchPoint : points) {
+                fileWriter.write("" + touchPoint.x + " " + touchPoint.y + newLine);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            FileUtils.closeQuietly(fileWriter);
+            Log.e("###########", "save points finished");
+        }
+    }
 
-        final int viewLocation[] = {0, 0};
-        surfaceView.getLocationOnScreen(viewLocation);
-        final Matrix viewMatrix = new Matrix();
-        DeviceConfig deviceConfig = DeviceConfig.sharedInstance(this, "note");
-        viewMatrix.postRotate(deviceConfig.getViewPostOrientation());
-        viewMatrix.postTranslate(deviceConfig.getViewPostTx(), deviceConfig.getViewPostTy());
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
-        float screenPoints[] = {viewLocation[0] + x, viewLocation[1] + y};
-        float dst[] = {0, 0};
-        viewMatrix.mapPoints(dst, screenPoints);
-        return dst;
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 
 }
