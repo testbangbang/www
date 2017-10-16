853e8533b3fbca0b5cac46259b7decf116f5971e
jim <qiujiejun@onyx-international.com>
2017-10-16 11:24:51 +0800

    clean code.

--- android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/ReaderSetDocumentTitleService.java
+++ android/kreader/app/kreader/src/main/java/com/onyx/kreader/ui/ReaderSetDocumentTitleService.java
@@ -4,14 +4,11 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.util.Log;
 
 import com.onyx.android.sdk.reader.ISetDocumentTitleService;
 import com.onyx.android.sdk.reader.utils.PdfWriterUtils;
 
 public class ReaderSetDocumentTitleService extends Service {
-
-    private final static String TAG = ReaderSetDocumentTitleService.class.getSimpleName();
 
     ISetDocumentTitleService.Stub mBinder = new ISetDocumentTitleService.Stub() {
         @Override
@@ -29,7 +26,6 @@ public class ReaderSetDocumentTitleService extends Service {
     }
 
     private boolean copy(String path, String titleToSet) {
-        Log.d(TAG, "#### copy document, path: " + path + ", title to set: " + titleToSet);
         PdfWriterUtils.setDocumentTitle(path, titleToSet);
         return true;
     }
