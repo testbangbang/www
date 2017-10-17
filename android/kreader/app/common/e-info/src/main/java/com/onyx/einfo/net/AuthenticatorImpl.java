c3b2f7f06e990aba045f10f81f21fba0576d8cc5
wang.suicheng <wsc@onyx-international.com>
2017-10-17 15:41:41 +0800

    e-info: add AuthenticatorImpl function to relogin when token has invalid

--- android/kreader/app/common/e-info/src/main/java/com/onyx/einfo/net/AuthenticatorImpl.java
+++ android/kreader/app/common/e-info/src/main/java/com/onyx/einfo/net/AuthenticatorImpl.java
@@ -1,10 +1,16 @@
 package com.onyx.einfo.net;
 
 import android.content.Context;
+import android.util.Log;
+
 import com.onyx.android.sdk.data.CloudManager;
+import com.onyx.android.sdk.data.Constant;
 import com.onyx.android.sdk.data.db.table.EduAccountProvider;
 import com.onyx.android.sdk.data.model.v2.EduAccount;
+import com.onyx.android.sdk.data.model.v2.NeoAccountBase;
 import com.onyx.android.sdk.data.request.cloud.v2.LoginByHardwareInfoRequest;
+import com.onyx.android.sdk.data.v2.ContentService;
+import com.onyx.einfo.InfoApp;
 
 import java.io.IOException;
 
@@ -18,28 +24,37 @@ import okhttp3.Route;
  */
 
 public class AuthenticatorImpl implements Authenticator {
+    private static final String TAG = "AuthenticatorImpl";
 
-    private Context context;
+    private Context appContext;
     private CloudManager cloudManager;
 
-    public AuthenticatorImpl(Context context, CloudManager cloudManager) {
-        this.context = context;
+    public AuthenticatorImpl(Context appContext, CloudManager cloudManager) {
+        this.appContext = appContext.getApplicationContext();
         this.cloudManager = cloudManager;
     }
 
     @Override
     public Request authenticate(Route route, Response response) throws IOException {
+        Log.e(TAG, "execute AuthenticatorImpl");
+        Request.Builder builder = response.request().newBuilder();
         final LoginByHardwareInfoRequest accountLoadRequest = new LoginByHardwareInfoRequest<>(EduAccountProvider.CONTENT_URI,
                 EduAccount.class);
         accountLoadRequest.setLoadOnlyFromCloud(true);
+        accountLoadRequest.setContext(appContext);
         try {
             accountLoadRequest.execute(cloudManager);
-            if (accountLoadRequest.getAccount() == null) {
+            NeoAccountBase account = accountLoadRequest.getAccount();
+            if (!NeoAccountBase.isValid(account) || accountLoadRequest.getException() != null) {
                 return null;
             }
+            builder.removeHeader(Constant.HEADER_AUTHORIZATION);
+            builder.addHeader(Constant.HEADER_AUTHORIZATION,
+                    ContentService.CONTENT_AUTH_PREFIX + cloudManager.getToken());
         } catch (Exception e) {
+            Log.e(TAG, "exec authenticate", e);
             return null;
         }
-        return response.request().newBuilder().build();
+        return builder.build();
     }
 }
