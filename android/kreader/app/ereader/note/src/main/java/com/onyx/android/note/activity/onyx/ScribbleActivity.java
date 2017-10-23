9c94732e3c972c4ddd077d9979bd18c09e6f4d99
xiaomingz <351062367@qq.com>
2017-10-23 10:33:16 +0800

    fix menu view can not show when switch to span layout

--- android/kreader/app/ereader/note/src/main/java/com/onyx/android/note/activity/onyx/ScribbleActivity.java
+++ android/kreader/app/ereader/note/src/main/java/com/onyx/android/note/activity/onyx/ScribbleActivity.java
@@ -261,9 +261,13 @@ public class ScribbleActivity extends BaseScribbleActivity {
         switchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                syncWithCallback(true, true, null);
-                toggleLineLayoutMode();
-                switchScribbleMode(isLineLayoutMode());
+                syncWithCallback(true, true, new BaseCallback() {
+                    @Override
+                    public void done(BaseRequest request, Throwable e) {
+                        toggleLineLayoutMode();
+                        switchScribbleMode(isLineLayoutMode());
+                    }
+                });
             }
         });
 
