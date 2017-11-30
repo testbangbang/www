1f721959a899522002cec9db510bb2a4236749e8
xiaomingz <351062367@qq.com>
2017-11-30 11:10:15 +0800

    fix can not normal start stop scribble

--- android/kreader/app/edu/edu-reader/src/main/java/com/onyx/edu/reader/note/request/StartNoteRequest.java
+++ android/kreader/app/edu/edu-reader/src/main/java/com/onyx/edu/reader/note/request/StartNoteRequest.java
@@ -2,6 +2,7 @@ package com.onyx.edu.reader.note.request;
 
 import com.onyx.android.sdk.data.PageInfo;
 import com.onyx.android.sdk.scribble.data.NoteDrawingArgs;
+import com.onyx.android.sdk.scribble.shape.ShapeFactory;
 import com.onyx.edu.reader.note.NoteManager;
 
 import java.util.List;
@@ -20,13 +21,21 @@ public class StartNoteRequest extends ReaderBaseNoteRequest {
 
     public void execute(final NoteManager noteManager) throws Exception {
         ensureDocumentOpened(noteManager);
-        setVisiblePages(getVisiblePages());
+        noteManager.setVisiblePages(getVisiblePages());
         noteManager.startRawEventProcessor();
+        noteManager.resumeRawEventProcessor(getContext());
         noteManager.enableRawEventProcessor(true);
-        noteManager.setCurrentShapeType(noteManager.getNoteDocument().getCurrentShapeType());
-        noteManager.setCurrentShapeColor(noteManager.getNoteDocument().getStrokeColor());
+        // we don't want current shape to be eraser when entering note writing mode,
+        // which is confusing
+        noteManager.setCurrentShapeType(noteManager.isEraser() ?
+                ShapeFactory.SHAPE_PENCIL_SCRIBBLE :
+                noteManager.getNoteDocument().getCurrentShapeType());
+        noteManager.setCurrentShapeColor(NoteDrawingArgs.defaultColor());
+        noteManager.restoreStrokeWidth();
         noteManager.setNoteDirty(true);
         getNoteDataInfo().setContentRendered(renderVisiblePages(noteManager));
+        setResumeRawInputProcessor(noteManager.isDFBForCurrentShape());
     }
+
 
 }
