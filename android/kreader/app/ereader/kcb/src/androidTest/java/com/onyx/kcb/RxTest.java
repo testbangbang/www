efce632b4146687b2bd7c45732232b5e8a892a50
john <john@onyx-international.com>
2017-12-03 12:09:10 +0800

    add concat test.

--- android/kreader/app/ereader/kcb/src/androidTest/java/com/onyx/kcb/RxTest.java
+++ android/kreader/app/ereader/kcb/src/androidTest/java/com/onyx/kcb/RxTest.java
@@ -1,13 +1,17 @@
 package com.onyx.kcb;
 
 import android.test.ApplicationTestCase;
+import android.util.Log;
 
 import com.onyx.android.sdk.utils.TestUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
@@ -64,6 +68,43 @@ public class RxTest extends ApplicationTestCase<KCBApplication> {
                     countDownLatch.countDown();
                 }
             });
+        countDownLatch.await();
+    }
+
+    public void testConcat() throws Exception {
+        final int s1 = 1000;
+        final int s2 = 1500;
+        final List value = new ArrayList();
+        value.add(s1);
+        value.add(s2);
+        Observable<String> first = Observable.fromCallable(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                TestUtils.sleep(s1);
+                return String.valueOf(s1);
+            }
+        });
+
+        Observable<String> second = Observable.fromCallable(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                TestUtils.sleep(s2);
+                return String.valueOf(s2);
+            }
+        });
+
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        Observable.concat(first, second)
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String o) throws Exception {
+                String v = String.valueOf(value.remove(0));
+                assertEquals(v, o);
+                countDownLatch.countDown();
+            }
+        });
         countDownLatch.await();
     }
 }
