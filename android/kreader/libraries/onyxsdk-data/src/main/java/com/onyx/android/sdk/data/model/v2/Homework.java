cd26a8324ee964b782204c883f77a43848e09b29
wang.suicheng <wsc@onyx-international.com>
2017-12-17 12:34:29 +0800

    refactor(onyxsdk-data): redefine the homework model

--- android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/model/v2/Homework.java
+++ android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/model/v2/Homework.java
@@ -7,6 +7,7 @@ import com.onyx.android.sdk.data.model.BaseData;
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.Table;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,18 +16,15 @@ import java.util.List;
  * Created by suicheng on 2017/11/1.
  */
 @Table(database = ContentDatabase.class, allFields = true)
-public class Homework extends BaseData {
+public class Homework extends BaseData implements Serializable {
+
     public String _id;
     public String parent;
     public String ref;
     public String title;
-    public String info;
     @Column(typeConverter = ListStringConverter.class)
     public List<String> category = new ArrayList<>();
-    public String subject;
 
-    @Column(typeConverter = ListStringConverter.class)
-    public List<String> resources;
     @Column(typeConverter = HomeworkDetailConverter.class)
     public HomeworkDetail child;
 
@@ -37,10 +35,6 @@ public class Homework extends BaseData {
     public int QuesType;
     public boolean isCollect;
     public boolean published;
-    public boolean needReply;
-
-    public Date beginTime;
-    public Date endTime;
 
     @Override
     public void beforeSave() {
--- android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/model/v2/Subject.java
+++ android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/model/v2/Subject.java
@@ -2,10 +2,12 @@ package com.onyx.android.sdk.data.model.v2;
 
 import com.onyx.android.sdk.data.model.BaseData;
 
+import java.io.Serializable;
+
 /**
  * Created by suicheng on 2017/11/14.
  */
-public class Subject extends BaseData {
+public class Subject extends BaseData implements Serializable {
     public String _id;
     public String name;
 }
