15db3ec651ba11444b14287ab8dff638a0347740
wang.suicheng <wsc@onyx-international.com>
2017-12-17 12:34:29 +0800

    refactor(onyxsdk-data): redefine the homework model

--- android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/model/v2/HomeworkDetail.java
+++ android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/model/v2/HomeworkDetail.java
@@ -1,25 +1,33 @@
 package com.onyx.android.sdk.data.model.v2;
 
-import java.util.List;
+import com.onyx.android.sdk.data.model.BaseData;
+
+import java.io.Serializable;
+import java.util.Date;
 import java.util.Map;
 
 /**
  * Created by suicheng on 2017/12/7.
  */
-public class HomeworkDetail {
+public class HomeworkDetail extends BaseData implements Serializable {
+
     public String _id;
     public String title;
+    public String info;
+    public String type;
     public Subject subject;
     public NeoAccountBase creator;
-    public CloudMetadata file;
     public Map<String, Integer> difficultyCount;
     public Map<String, Integer> quesTypeCount;
-    public List<String> questions;
+
+    public Date beginTime;
+    public Date endTime;
+
+    public boolean needReply;
+
+    public int status;
 
     public String getSubjectName() {
-        if (subject == null) {
-            return "";
-        }
-        return subject.name;
+        return subject == null ? "" : subject.name;
     }
 }
