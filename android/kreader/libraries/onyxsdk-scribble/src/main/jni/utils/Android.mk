6ce2feb89a6a6a64236320c47e65326179979c53
Joy <joycodex@hotmail.com>
2017-12-23 20:49:19 +0800

    feat(onyxsdk-scribble): update BrushScribbleShape to use new ink_utils lib

--- android/kreader/libraries/onyxsdk-scribble/src/main/jni/utils/Android.mk
+++ android/kreader/libraries/onyxsdk-scribble/src/main/jni/utils/Android.mk
@@ -10,9 +10,25 @@ LOCAL_MODULE := libutils
 src_files := \
     JNIUtils.cpp
 
-
 LOCAL_CFLAGS += -O3 -DUSE_MMAP
 LOCAL_SRC_FILES := $(src_files)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ink_utils
+
+LOCAL_NDK_STL_VARIANT := gnustl_static
+LOCAL_CXXFLAGS := -fPIC -std=c++11
+LOCAL_CXXFLAGS += -O3 -fstrict-aliasing -fprefetch-loop-arrays -fexceptions
+LOCAL_CXXFLAGS += -Wno-non-virtual-dtor -Wall
+
+LOCAL_STATIC_LIBRARIES := libutils
+
+LOCAL_LDLIBS := -llog -lz -ljnigraphics
+
+LOCAL_SRC_FILES := \
+    ink_utils.cpp \
+
+include $(BUILD_SHARED_LIBRARY)
