ae3d1f2f4cfc4a416b2dd7319f25ee6ffb1cefa1
john <john@onyx-international.com>
2017-12-24 17:08:19 +0800

    clean up.

--- android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/utils/InkUtils.java
+++ android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/utils/InkUtils.java
@@ -37,7 +37,9 @@ public class InkUtils {
         }
     }
 
-    public static void drawStroke(Canvas canvas, Paint paint, List<TouchPoint> points, Matrix matrix) {
+    public static void drawStroke(final RenderContext renderContext,
+                                  List<TouchPoint> points,
+                                  final float maxTouchPressure) {
         float[] src = new float[2];
         float[] dst = new float[2];
         float[] array = new float[points.size() * 5];
@@ -45,16 +47,17 @@ public class InkUtils {
             int idx = 5 * i;
             src[0] = points.get(i).x;
             src[1] = points.get(i).y;
-            matrix.mapPoints(dst, src);
+            renderContext.matrix.mapPoints(dst, src);
 
             array[idx] = dst[0];
             array[idx + 1] = dst[1];
-            array[idx + 2] = points.get(i).pressure / 2047.0f;
+            array[idx + 2] = points.get(i).pressure / maxTouchPressure;
             array[idx + 3] = points.get(i).size;
             array[idx + 4] = points.get(i).timestamp;
         }
 
-        nativeDrawStroke(canvas, paint, array);
+        nativeDrawStroke(renderContext.canvas,
+                renderContext.paint, array);
     }
 
     public static void setPressureEntries(final List<MappingConfig.PressureEntry> list) {
