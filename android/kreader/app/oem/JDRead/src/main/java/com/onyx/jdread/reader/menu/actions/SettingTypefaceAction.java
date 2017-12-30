5dad37ee40aefc6797e44b77523e1a48b6d55def
huxiaomao <huxiaomao@onyx-international.com>
2017-12-30 15:37:24 +0800

    setting typeface

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/menu/actions/SettingTypefaceAction.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/menu/actions/SettingTypefaceAction.java
@@ -1,15 +1,36 @@
 package com.onyx.jdread.reader.menu.actions;
 
+import com.onyx.android.sdk.data.ReaderTextStyle;
+import com.onyx.android.sdk.rx.RxCallback;
 import com.onyx.jdread.reader.actions.BaseAction;
 import com.onyx.jdread.reader.data.ReaderDataHolder;
+import com.onyx.jdread.reader.menu.request.SettingTextStyleRequest;
 
 /**
  * Created by huxiaomao on 2017/12/30.
  */
 
-public class SettingTypefaceAction extends BaseAction{
+public class SettingTypefaceAction extends BaseAction {
+    private String typefacePath;
+
+    public SettingTypefaceAction(String typefacePath) {
+        this.typefacePath = typefacePath;
+    }
+
     @Override
     public void execute(ReaderDataHolder readerDataHolder) {
+        ReaderTextStyle style = readerDataHolder.getReader().getReaderHelper().getTextStyleManager().getStyle();
+        style.setFontFace(typefacePath);
 
+        new SettingTextStyleRequest(readerDataHolder).execute(new RxCallback() {
+            @Override
+            public void onNext(Object o) {
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+        });
     }
 }
