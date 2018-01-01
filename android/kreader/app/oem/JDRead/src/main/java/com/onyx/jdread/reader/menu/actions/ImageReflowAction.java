3fbd03451ca8d4113de7e2d2056ed946ee3bd7bd
huxiaomao <huxiaomao@onyx-international.com>
2018-01-01 16:13:58 +0800

    add Comic,Article mode

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/menu/actions/ImageReflowAction.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/menu/actions/ImageReflowAction.java
@@ -1,7 +1,10 @@
 package com.onyx.jdread.reader.menu.actions;
 
+import com.onyx.android.sdk.data.PageConstants;
+import com.onyx.android.sdk.reader.host.navigation.NavigationArgs;
 import com.onyx.android.sdk.rx.RxCallback;
 import com.onyx.jdread.reader.actions.BaseReaderAction;
+import com.onyx.jdread.reader.data.ChangeLayoutParameter;
 import com.onyx.jdread.reader.data.ReaderDataHolder;
 import com.onyx.jdread.reader.menu.request.ChangeLayoutRequest;
 
@@ -9,10 +12,12 @@ import com.onyx.jdread.reader.menu.request.ChangeLayoutRequest;
  * Created by huxiaomao on 2017/12/30.
  */
 
-public class ChangeLayoutAction extends BaseReaderAction {
+public class ImageReflowAction extends BaseReaderAction {
+
     @Override
     public void execute(final ReaderDataHolder readerDataHolder) {
-        new ChangeLayoutRequest(readerDataHolder).execute(new RxCallback() {
+        ChangeLayoutParameter parameter = new ChangeLayoutParameter(PageConstants.IMAGE_REFLOW_PAGE, new NavigationArgs());
+        new ChangeLayoutRequest(readerDataHolder,parameter).execute(new RxCallback() {
             @Override
             public void onNext(Object o) {
 
--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/menu/request/ChangeLayoutRequest.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/menu/request/ChangeLayoutRequest.java
@@ -1,7 +1,6 @@
 package com.onyx.jdread.reader.menu.request;
 
-import com.onyx.android.sdk.data.PageConstants;
-import com.onyx.android.sdk.reader.host.navigation.NavigationArgs;
+import com.onyx.jdread.reader.data.ChangeLayoutParameter;
 import com.onyx.jdread.reader.data.ReaderDataHolder;
 import com.onyx.jdread.reader.request.ReaderBaseRequest;
 
@@ -11,15 +10,17 @@ import com.onyx.jdread.reader.request.ReaderBaseRequest;
 
 public class ChangeLayoutRequest extends ReaderBaseRequest {
     private ReaderDataHolder readerDataHolder;
+    private ChangeLayoutParameter parameter;
 
-    public ChangeLayoutRequest(ReaderDataHolder readerDataHolder) {
+    public ChangeLayoutRequest(ReaderDataHolder readerDataHolder,ChangeLayoutParameter parameter) {
         this.readerDataHolder = readerDataHolder;
+        this.parameter = parameter;
     }
 
     @Override
     public ChangeLayoutRequest call() throws Exception {
         readerDataHolder.getReader().getReaderHelper().getReaderLayoutManager().setSavePosition(true);
-        readerDataHolder.getReader().getReaderHelper().getReaderLayoutManager().setCurrentLayout(PageConstants.IMAGE_REFLOW_PAGE, new NavigationArgs());
+        readerDataHolder.getReader().getReaderHelper().getReaderLayoutManager().setCurrentLayout(parameter.getLayout(), parameter.getNavigationArgs());
         readerDataHolder.getReader().getReaderViewHelper().updatePageView(readerDataHolder);
         return this;
     }
