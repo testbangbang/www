bfad58a3f6d4fd810ed27f59984efbf2ecaf350f
Joy <joycodex@hotmail.com>
2018-01-05 12:04:33 +0800

    feat(onyxsdk-scribble): switch default shape to ShapeFactory.SHAPE_BRUSH_SCRIBBLE

--- android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/data/NoteDrawingArgs.java
+++ android/kreader/libraries/onyxsdk-scribble/src/main/java/com/onyx/android/sdk/scribble/data/NoteDrawingArgs.java
@@ -44,7 +44,7 @@ public class NoteDrawingArgs {
     }
 
     public static int defaultShape() {
-        return ShapeFactory.SHAPE_PENCIL_SCRIBBLE;
+        return ShapeFactory.SHAPE_BRUSH_SCRIBBLE;
     }
 
     public static int defaultColor() {
