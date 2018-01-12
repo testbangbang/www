8cb637152fe26caca78a2bdb0b3fcaf878dd8b19
Solskjaer49 <solskjaer49@gmail.com>
2018-01-12 18:49:11 +0800

    fix(note): fix note menu update exclude region will cause multiple sync issue.

---
+++ android/kreader/app/ereader/note/src/main/java/com/onyx/android/note/actions/scribble/UpdateScreenWritingExcludeRegionAction.java
@@ -0,0 +1,30 @@
+package com.onyx.android.note.actions.scribble;
+
+import android.graphics.Rect;
+
+import com.onyx.android.note.actions.BaseNoteAction;
+import com.onyx.android.note.activity.BaseScribbleActivity;
+import com.onyx.android.sdk.common.request.BaseCallback;
+import com.onyx.android.sdk.scribble.request.shape.UpdateScreenWritingExcludeRegionRequest;
+
+/**
+ * Created by solskjaer49 on 2018/1/12 17:59.
+ */
+
+public class UpdateScreenWritingExcludeRegionAction<T extends BaseScribbleActivity> extends BaseNoteAction<T> {
+    private UpdateScreenWritingExcludeRegionRequest updateExcludeRegionRequest;
+
+    public UpdateScreenWritingExcludeRegionAction(Rect excludeRect, boolean resumeDrawing) {
+        this.resumeDrawing = resumeDrawing;
+        this.excludeRect = excludeRect;
+    }
+
+    private boolean resumeDrawing;
+    private Rect excludeRect;
+
+    @Override
+    public void execute(T activity, BaseCallback callback) {
+        updateExcludeRegionRequest = new UpdateScreenWritingExcludeRegionRequest(excludeRect, resumeDrawing);
+        activity.submitRequest(updateExcludeRegionRequest, callback);
+    }
+}
