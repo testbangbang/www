1933f9fa0f516a23a66b91f4a1f322fb5e844ad8
xiaomingz <351062367@qq.com>
2018-01-16 15:07:55 +0800

    fix(edu-release-1.0): adjust playaudiorequest code

--- android/kreader/libraries/onyxsdk-reader/src/main/java/com/onyx/android/sdk/reader/host/request/PlayAudioRequest.java
+++ android/kreader/libraries/onyxsdk-reader/src/main/java/com/onyx/android/sdk/reader/host/request/PlayAudioRequest.java
@@ -1,14 +1,18 @@
 package com.onyx.android.sdk.reader.host.request;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 
 import com.onyx.android.sdk.reader.common.BaseReaderRequest;
 import com.onyx.android.sdk.reader.host.wrapper.Reader;
+import com.onyx.android.sdk.utils.FileUtils;
 
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * Created by lxm on 2018/1/16.
@@ -35,13 +39,27 @@ public class PlayAudioRequest extends BaseReaderRequest {
         mediaPlayer.start();
     }
 
-    private FileDescriptor saveToTempFile() throws Exception {
-        File tempMp3 = File.createTempFile("temp", "mp3");
-        tempMp3.deleteOnExit();
-        FileOutputStream fos = new FileOutputStream(tempMp3);
-        fos.write(data);
-        fos.close();
-        FileInputStream fis = new FileInputStream(tempMp3);
-        return fis.getFD();
+    private String saveToTempFile() {
+        String path = null;
+        File tempMp3 = null;
+        FileOutputStream fos = null;
+        try {
+            File dir = getContext().getDir("temp_audio", Context.MODE_PRIVATE);
+            FileUtils.purgeDirectory(dir);
+            tempMp3 = File.createTempFile(UUID.randomUUID().toString(), "mp3", dir);
+            tempMp3.deleteOnExit();
+            fos = new FileOutputStream(tempMp3);
+            fos.write(data);
+            fos.close();
+            path = tempMp3.getAbsolutePath();
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (tempMp3 != null) {
+                tempMp3.delete();
+            }
+        }finally {
+            FileUtils.closeQuietly(fos);
+        }
+        return path;
     }
 }
