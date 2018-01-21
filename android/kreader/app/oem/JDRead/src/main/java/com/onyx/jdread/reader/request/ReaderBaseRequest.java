492fd2965add7084f80f261ae07b0112a93eea3f
huxiaomao <huxiaomao@onyx-international.com>
2018-01-21 18:50:35 +0800

    add ReaderSchedulers

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/request/ReaderBaseRequest.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/request/ReaderBaseRequest.java
@@ -1,21 +1,16 @@
 package com.onyx.jdread.reader.request;
 
-import com.onyx.android.sdk.common.request.ExecutorContext;
 import com.onyx.android.sdk.reader.common.ReaderViewInfo;
 import com.onyx.android.sdk.rx.RxRequest;
 import com.onyx.jdread.reader.common.ReaderUserDataInfo;
 
-import java.util.concurrent.ExecutorService;
-
 import io.reactivex.Scheduler;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by huxiaomao on 2017/12/26.
  */
 
 public abstract class ReaderBaseRequest extends RxRequest {
-    static final ExecutorService executorService = new ExecutorContext().getSingleThreadPool();
     private ReaderViewInfo readerViewInfo;
     private ReaderUserDataInfo readerUserDataInfo;
     public boolean isSuccess = true;
@@ -33,7 +28,7 @@ public abstract class ReaderBaseRequest extends RxRequest {
     }
 
     public ReaderViewInfo getReaderViewInfo() {
-        if(readerViewInfo == null) {
+        if (readerViewInfo == null) {
             readerViewInfo = new ReaderViewInfo();
         }
         return readerViewInfo;
@@ -41,6 +36,6 @@ public abstract class ReaderBaseRequest extends RxRequest {
 
     @Override
     public Scheduler subscribeScheduler() {
-        return Schedulers.from(executorService);
+        return ReaderSchedulers.readerScheduler();
     }
 }
