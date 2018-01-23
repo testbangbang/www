e2bd0c8a624bbb9d5c40b094feb7341e407a1ec1
hehai <hehai@onyx-international.com>
2018-01-23 22:17:03 +0800

    save cloud book thumbnail.

--- android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/rxrequest/data/db/RxLibraryLoadRequest.java
+++ android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/rxrequest/data/db/RxLibraryLoadRequest.java
@@ -79,9 +79,12 @@ public class RxLibraryLoadRequest extends RxBaseDBRequest {
 
         totalCount = getDataProvider().count(getAppContext(), queryArgs) + getDataProvider().libraryCount(queryArgs.libraryUniqueId);
         if (loadMetadata && libraryList.size() < queryArgs.limit) {
-            queryArgs.offset = (int) (queryArgs.offset - getDataProvider().libraryCount(queryArgs.libraryUniqueId));
+            queryArgs.offset = (int) (queryArgs.offset - getDataProvider().libraryCount(this.queryArgs.libraryUniqueId));
+            int limit = queryArgs.limit;
+            queryArgs.limit = queryArgs.limit -libraryList.size();
             List<Metadata> metadataList = DataManagerHelper.loadMetadataListWithCache(getAppContext(), getDataManager(),
-                    queryArgs, loadFromCache);
+                    this.queryArgs, loadFromCache);
+            queryArgs.limit = limit;
             if (!CollectionUtils.isNullOrEmpty(metadataList)) {
                 bookList.addAll(metadataList);
                 loadBitmaps(getAppContext(), getDataManager());
