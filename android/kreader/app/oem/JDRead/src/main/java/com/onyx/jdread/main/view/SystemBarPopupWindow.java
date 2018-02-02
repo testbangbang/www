f2a8440486c095663b63adfc5274d48b4f535afd
hehai <hehai@onyx-international.com>
2018-02-02 20:13:22 +0800

    fix speed refresh display name.

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/main/view/SystemBarPopupWindow.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/main/view/SystemBarPopupWindow.java
@@ -22,6 +22,8 @@ import com.onyx.android.sdk.utils.NetworkUtil;
 import com.onyx.jdread.JDReadApplication;
 import com.onyx.jdread.R;
 import com.onyx.jdread.databinding.SystemBarPopLayoutBinding;
+import com.onyx.jdread.main.common.ResManager;
+import com.onyx.jdread.main.common.ToastUtil;
 import com.onyx.jdread.main.event.SystemBarBackToSettingEvent;
 import com.onyx.jdread.setting.model.BrightnessModel;
 
@@ -103,6 +105,7 @@ public class SystemBarPopupWindow extends PopupWindow {
 
         public void toggleA2Model() {
             boolean useFastMode = !EpdController.inSystemFastMode();
+            ToastUtil.showToast(useFastMode? ResManager.getString(R.string.speed_refresh_is_opened):ResManager.getString(R.string.speed_refresh_is_closed));
             speedRefresh.set(useFastMode);
             if (useFastMode) {
                 EpdController.setSystemUpdateModeAndScheme(UpdateMode.ANIMATION, UpdateScheme.QUEUE_AND_MERGE, Integer.MAX_VALUE);
