ba2cea485a11ebc944b2c8d19dd0bd89b0ac3460
john <zeng.zhu@gmail.com>
2018-02-02 22:15:20 +0800

    clean up and add preload.

---
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/request/PreloadPreviousScreenRequest.java
@@ -0,0 +1,29 @@
+package com.onyx.jdread.reader.request;
+
+import com.onyx.android.sdk.reader.common.ReaderDrawContext;
+import com.onyx.jdread.reader.data.Reader;
+
+/**
+ * Created by zhuzeng on 02/02/2018.
+ */
+
+public class PreloadPreviousScreenRequest extends ReaderBaseRequest {
+    private Reader reader;
+
+    public PreloadPreviousScreenRequest(Reader reader) {
+        this.reader = reader;
+    }
+
+    @Override
+    public PreloadPreviousScreenRequest call() throws Exception {
+        reader.getReaderHelper().getReaderLayoutManager().setSavePosition(false);
+        if (reader.getReaderHelper().previousScreen()) {
+            ReaderDrawContext context = ReaderDrawContext.create(false);
+            reader.getReaderHelper().getReaderLayoutManager().drawVisiblePages(reader, context, getReaderViewInfo());
+            reader.getReaderHelper().saveToCache(context.renderingBitmap);
+            reader.getReaderHelper().nextScreen();
+        }
+        return this;
+    }
+
+}
