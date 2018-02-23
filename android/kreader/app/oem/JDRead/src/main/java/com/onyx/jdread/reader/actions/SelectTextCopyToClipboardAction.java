93656f4006dfa1c3e54404a3b4d5c6c6d93b8172
huxiaomao <huxiaomao@onyx-international.com>
2018-02-23 22:04:29 +0800

    fix copy text update page

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/actions/SelectTextCopyToClipboardAction.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/actions/SelectTextCopyToClipboardAction.java
@@ -3,6 +3,7 @@ package com.onyx.jdread.reader.actions;
 import android.content.ClipboardManager;
 import android.content.Context;
 
+import com.onyx.android.sdk.api.device.epd.EpdController;
 import com.onyx.android.sdk.rx.RxCallback;
 import com.onyx.jdread.R;
 import com.onyx.jdread.main.common.ToastUtil;
@@ -14,10 +15,22 @@ import com.onyx.jdread.reader.data.ReaderDataHolder;
  */
 
 public class SelectTextCopyToClipboardAction extends BaseReaderAction {
+    private String text;
     @Override
-    public void execute(ReaderDataHolder readerDataHolder, RxCallback baseCallback) {
-        String text = readerDataHolder.getReaderSelectionInfo().getSelectText();
+    public void execute(final ReaderDataHolder readerDataHolder, RxCallback baseCallback) {
+        text = readerDataHolder.getReaderSelectionInfo().getSelectText();
 
+        new CleanSelectionAction().execute(readerDataHolder, new RxCallback() {
+            @Override
+            public void onNext(Object o) {
+                showMessage(readerDataHolder);
+            }
+        });
+
+
+    }
+
+    private void showMessage(ReaderDataHolder readerDataHolder){
         ClipboardManager clipboardManager = (ClipboardManager) readerDataHolder.getAppContext().getSystemService(Context.CLIPBOARD_SERVICE);
         clipboardManager.setText(text);
         ToastMessage.showMessageCenter(readerDataHolder.getAppContext(),readerDataHolder.getAppContext().getString(R.string.reader_copy_success));
