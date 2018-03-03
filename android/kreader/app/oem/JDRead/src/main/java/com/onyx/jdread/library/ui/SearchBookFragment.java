db3af671dc8355bb67254d58d4107b6206db604d
huxiaomao <huxiaomao@onyx-international.com>
2018-03-03 14:49:04 +0800

    add open book key

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/library/ui/SearchBookFragment.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/library/ui/SearchBookFragment.java
@@ -1,6 +1,7 @@
 package com.onyx.jdread.library.ui;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.SearchView;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jingdong.app.reader.data.DrmTools;
 import com.onyx.android.sdk.data.GPaginator;
 import com.onyx.android.sdk.data.event.ItemClickEvent;
 import com.onyx.android.sdk.data.model.DataModel;
@@ -410,6 +412,11 @@ public class SearchBookFragment extends BaseFragment {
         }
         DocumentInfo documentInfo = new DocumentInfo();
         documentInfo.setBookPath(filePath);
+        DocumentInfo.SecurityInfo securityInfo = new DocumentInfo.SecurityInfo();
+        securityInfo.setKey(model.key.get());
+        securityInfo.setRandom(model.random.get());
+        securityInfo.setUuId(DrmTools.getHardwareId(Build.SERIAL));
+        documentInfo.setSecurityInfo(securityInfo);
         OpenBookHelper.openBook(getContext(), documentInfo);
     }
 
---
+++ android/kreader/libraries/onyxsdk-data/src/main/java/com/onyx/android/sdk/data/model/BookExtraInfoBean.java
@@ -0,0 +1,17 @@
+package com.onyx.android.sdk.data.model;
+
+/*
+ * create by jackdeng 2017/12/23 14:49
+ */
+
+public class BookExtraInfoBean {
+    public boolean isWholeBookDownLoad;
+    public int percentage;
+    public int downLoadState;
+    public String localPath;
+    public Object downLoadTaskTag;
+    public float totalSize;
+    public float progress;
+    public String key;
+    public String random;
+}
