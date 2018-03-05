d711e311e77ed7c5f680652cd18989279dc4dd7d
huxiaomao <huxiaomao@onyx-international.com>
2018-03-05 19:10:33 +0800

    adjust preload file path

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/actions/CheckPreloadBookStateAction.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/actions/CheckPreloadBookStateAction.java
@@ -8,6 +8,8 @@ import com.onyx.android.sdk.utils.FileUtils;
 import com.onyx.jdread.reader.data.ReaderDataHolder;
 import com.onyx.jdread.reader.utils.ReaderViewUtil;
 
+import java.io.File;
+
 /**
  * Created by huxiaomao on 2018/2/12.
  */
@@ -15,26 +17,23 @@ import com.onyx.jdread.reader.utils.ReaderViewUtil;
 public class CheckPreloadBookStateAction extends BaseReaderAction {
     private String preloadBook;
     private static final String PRELOAD_BOOK_NAME = "preload.txt";
-    private static final String PRELOAD_BOOK_DIRECTORY = "/sdcard/.preload/";
-    private static final String PRELOAD_BOOK_PATH = PRELOAD_BOOK_DIRECTORY + PRELOAD_BOOK_NAME;
+
     @Override
     public void execute(ReaderDataHolder readerDataHolder, RxCallback baseCallback) {
-        if(!FileUtils.fileExist(PRELOAD_BOOK_PATH)){
-            if(!FileUtils.fileExist(PRELOAD_BOOK_DIRECTORY)){
-                FileUtils.mkdirs(PRELOAD_BOOK_DIRECTORY);
-            }
-            createPreloadBook(readerDataHolder.getAppContext());
+        String preloadPath = readerDataHolder.getAppContext().getFilesDir().getAbsolutePath() + File.separator;
+        String bookPath = preloadPath + PRELOAD_BOOK_NAME;
+        if(!FileUtils.fileExist(bookPath)){
+            createPreloadBook(readerDataHolder.getAppContext(),bookPath);
         }
-        preloadBook = PRELOAD_BOOK_PATH;
+        preloadBook = bookPath;
         return;
     }
 
-    public static void createPreloadBook(Context context) {
+    public static void createPreloadBook(Context context,String preloadPath) {
         String srcFileName = PRELOAD_BOOK_NAME;
-        String destPath = PRELOAD_BOOK_PATH;
 
         AssetManager am = context.getAssets();
-        ReaderViewUtil.readAssetsFile(am, srcFileName, destPath);
+        ReaderViewUtil.readAssetsFile(am, srcFileName, preloadPath);
     }
 
     public String getPreloadBook() {
