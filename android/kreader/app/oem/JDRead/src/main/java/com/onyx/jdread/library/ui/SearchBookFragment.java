a5a9395251ed9229898d816d068241ce218ab61b
wang.suicheng <wsc@onyx-international.com>
2018-03-12 14:47:18 +0800

    fix(jdread): Bug #1330 SearchFragment uses the NetworkErrorFragment

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/library/ui/SearchBookFragment.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/library/ui/SearchBookFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.SearchView;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -246,10 +245,7 @@ public class SearchBookFragment extends BaseFragment {
 
     private void checkSearchResult() {
         if (isEmptySearchResults()) {
-            if (isWifiDisconnected()) {
-                return;
-            }
-            ToastUtil.showToast(R.string.no_search_results);
+            checkWifi(getSearchBookModel().searchKey.get());
         }
     }
 
