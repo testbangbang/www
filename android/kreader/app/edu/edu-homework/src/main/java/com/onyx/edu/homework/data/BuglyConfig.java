2f0a170065aba333fa7073deb0b6443877693bde
liaolintao <1413920792@qq.com>
2018-03-13 16:55:38 +0800

    feat(CrashReport): add bugly

---
+++ android/kreader/app/edu/edu-homework/src/main/java/com/onyx/edu/homework/data/BuglyConfig.java
@@ -0,0 +1,13 @@
+package com.onyx.edu.homework.data;
+
+/**
+ * <pre>
+ *     author : liao lin tao
+ *     time   : 2018/3/12 15:46
+ *     desc   :
+ * </pre>
+ */
+
+public interface BuglyConfig {
+    String APP_ID = "84af4fbd96";
+}
