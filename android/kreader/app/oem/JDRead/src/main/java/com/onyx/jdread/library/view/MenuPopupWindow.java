3307d70fb7a7d86d30f953dd6b8cbfebe4c447dc
MingBoLin <lmb@onyx-international.com>
2018-03-13 22:32:26 +0800

    Modify the location of my book drop-down menu.

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/library/view/MenuPopupWindow.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/library/view/MenuPopupWindow.java
@@ -54,7 +54,7 @@ public class MenuPopupWindow extends PopupWindow {
         popMenuAdapter.setData(list, list.size(), 1);
         int[] location = new int[2];
         view.getLocationOnScreen(location);
-        showAtLocation(view, Gravity.NO_GRAVITY, 770, y + location[1] + view.getHeight());
+        showAtLocation(view, Gravity.NO_GRAVITY, R.integer.personal_book_dropdown_menu, y + location[1] + view.getHeight());
     }
 
     private void initView() {
