4a435ae561336663e267e66d28ac6740e28b0b26
Joy <joycodex@hotmail.com>
2018-03-15 15:12:41 +0800

    fix(JDRead): let ReaderSettingMenuDialog to extends OnyxBaseDialog, which will enable/disable regal mode automatically

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/menu/dialog/ReaderSettingMenuDialog.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/reader/menu/dialog/ReaderSettingMenuDialog.java
@@ -11,6 +11,7 @@ import android.widget.RatingBar;
 import android.widget.SeekBar;
 
 import com.onyx.android.sdk.rx.RxCallback;
+import com.onyx.android.sdk.ui.dialog.OnyxBaseDialog;
 import com.onyx.android.sdk.ui.view.DisableScrollGridManager;
 import com.onyx.android.sdk.ui.view.PageRecyclerView;
 import com.onyx.android.sdk.utils.DeviceUtils;
@@ -40,7 +41,7 @@ import com.onyx.jdread.setting.model.BrightnessModel;
  * Created by huxiaomao on 17/5/10.
  */
 
-public class ReaderSettingMenuDialog extends Dialog implements ReaderSettingViewBack{
+public class ReaderSettingMenuDialog extends OnyxBaseDialog implements ReaderSettingViewBack{
     private static final String TAG = ReaderSettingMenuDialog.class.getSimpleName();
     private ReaderSettingMenuBinding binding;
     private ReaderDataHolder readerDataHolder;
