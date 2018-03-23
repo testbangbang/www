a71a06150c115ff9ec4e2a410d9cfc6d9363e4b4
lmb <lmb@onyx-international.com>
2018-03-23 12:57:26 +0800

    modify Viewing the comment content will appear in the cache of the previous book.

--- android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/shop/ui/CommentFragment.java
+++ android/kreader/app/oem/JDRead/src/main/java/com/onyx/jdread/shop/ui/CommentFragment.java
@@ -2,7 +2,6 @@ package com.onyx.jdread.shop.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
